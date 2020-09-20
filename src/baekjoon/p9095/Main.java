e2380469ecfb95db5c6fe022c0ea670c63573c83
Sion <ygonepiece@naver.com>
2020-09-20 20:29:59 +0900

    1, 2, 3 더하기(9095) 성공

---
+++ src/baekjoon/p9095/Main.java
@@ -0,0 +1,30 @@
+package baekjoon.p9095;
+
+import java.util.Scanner;
+
+public class Main {
+	static int[] cache = new int[11 + 1];
+	
+	static int dp(int N) {
+		if(N < 0) {return 0;}
+		if(N == 0) {return 1;}
+		if(cache[N]!=0) {return cache[N];}
+		
+		int sum = 0;
+		for(int i = 1; i <= 3; i++) {
+			sum += dp(N - i);
+		}
+		return cache[N] = sum;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = Integer.parseInt(sc.nextLine());
+		for (int i = 0; i < T; i++) {
+			int N = Integer.parseInt(sc.nextLine());
+			System.out.println(dp(N));
+		}
+		sc.close();
+	}
+
+}
