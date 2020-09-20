405e0faa87c0e9dab89ff5e49caa2580dcfb5009
Sion <ygonepiece@naver.com>
2020-09-20 14:12:38 +0900

    수 묶기(1744) 정답

--- src/baekjoon/p1744/Main.java
+++ src/baekjoon/p1744/Main.java
@@ -1,10 +1,7 @@
 package baekjoon.p1744;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
 	private static int[] S;
@@ -15,47 +12,35 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		S = new int[N];
 		for (int i = 0; i < N; i++) {
-			S[i] = new Random().nextInt(10001);
-			if (new Random().nextBoolean()) {
-				S[i] = -1 * S[i];
-			}
-			if (new Random().nextInt(5) == 0) {
-				S[i] = 0;
-			}
-			// S[i] = Integer.parseInt(br.readLine());
+			S[i] = Integer.parseInt(br.readLine());
 		}
 		br.close();
 		Arrays.sort(S);
-		System.out.println(Arrays.toString(S));
 
 		// [0, f)은 이미 순회한 상태
 		int f = 0;
 		long sum = 0;
-		System.out.println("앞에서 부터 : ");
 		while (f + 1 < N && S[f] < 0 && S[f + 1] < 0) {
-			System.out.println(S[f] + ", " + S[f + 1] + "묶음");
 			sum += (S[f] * S[f + 1]);
 			f += 2;
 		}
 		// 짝 지어지지 않은 음수와 0을 묶어준다
 		if (f + 1 < N && S[f] < 0 && S[f + 1] == 0) {
-			System.out.println(S[f] + ", " + S[f + 1] + "묶음");
 			f += 2;
 		}
 
-		// (b, N)은 이미 순회한 상태
+		// (b, N - 1]은 이미 순회한 상태
 		int b = N - 1;
-		System.out.println("뒤에서 부터 : ");
-		while (f <= b - 1 && S[b - 1] > 0 && S[b] > 0) {
-			System.out.println(S[b - 1] + ", " + S[b] + "묶음");
+		while (f <= b - 1 && S[b - 1] > 1 && S[b] > 1) {
 			sum += (S[b - 1] * S[b]);
 			b -= 2;
 		}
-
+		
+		// 순회하면서 묶지 않은 [f, b]를 더해준다
 		for (int i = f; i <= b; i++) {
-			System.out.println(S[i] +" 더함");
 			sum += S[i];
 		}
+
 		System.out.println(sum);
 	}
 
