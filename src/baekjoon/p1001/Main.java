201df7439f5e1fde45ad17df840a64e4f312f820
Sion <ygonepiece@naver.com>
2020-09-20 17:35:01 +0900

    A-B(1001) 정답

---
+++ src/baekjoon/p1001/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.p1001;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		br.close();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write((Integer.parseInt(st.nextToken()) - Integer.parseInt(st.nextToken())) +"");
+		bw.newLine();
+		bw.close();
+	}
+
+}
