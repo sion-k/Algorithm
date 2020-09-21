19849e51a4e6f2c7b00722883b2f09a6e05edf66
Sion <ygonepiece@naver.com>
2020-09-21 20:15:59 +0900

    소수 찾기(1978) 성공

--- src/baekjoon/p1978/Main.java
+++ src/baekjoon/p1978/Main.java
@@ -1,31 +1,22 @@
 package baekjoon.p1978;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		// -1 미계산 0 소수가 아님 1 소수
-		int[] S = new int[1000 + 1];
-		Arrays.fill(S, -1);
-		S[1] = 0;
-		for (int i = 2; i <= 1000; i++) {
-			if(S[i] != -1) {continue;}
-			S[i] = 1;
-			int k = i * 2;
-			while(k <= 1000) {
-				S[k] = 0;
-				k += i;
-			}
+	private static boolean isPrime(int N) {
+		if (N == 1 )return false;
+		for (int d = 2; d < N; d++) {
+			if (N % d == 0)
+				return false;
 		}
+		return true;
+	}
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		//int N = sc.nextInt();
+		int N = sc.nextInt();
 		int prime = 0;
-		for(int i = 1; i <= 1000; i++) {
-			if(S[i] == 1) {
-				System.out.print(i + " ");
-			}
-			//if(S[sc.nextInt()] == 1) {prime++;}
+		for (int i = 0; i < N; i++) {
+			if (isPrime(sc.nextInt())) {prime++;}
 		}
 		sc.close();
 		System.out.println(prime);
