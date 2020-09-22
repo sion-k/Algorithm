0544b8da9ead3483fd70cb15519aadb8e121f068
Sion <ygonepiece@naver.com>
2020-09-22 17:28:26 +0900

    구구단(2739) 성공

--- src/baekjoon/p2739/Main.java
+++ src/baekjoon/p2739/Main.java
@@ -1,10 +1,16 @@
 package baekjoon.p2739;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(N + " * " + i + " = " + N * i);
+		}
 	}
 
 }
