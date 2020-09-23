605b7d6235b3ff6905a97650a992ad0a4bafcb59
Sion <ygonepiece@naver.com>
2020-09-23 15:37:24 +0900

    별(16505) 성공

---
+++ src/baekjoon/p11726/Main.java
@@ -0,0 +1,24 @@
+package baekjoon.p11726;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int[] cache;
+
+	static int dp(int N) {
+		if (N <= 2) {return N;}
+		if (cache[N] != -1) {return cache[N];}
+		return cache[N] = (dp(N - 1) + dp(N - 2)) % 10007;
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		cache = new int[N + 1];
+		Arrays.fill(cache, -1);
+		System.out.println(dp(N));
+	}
+
+}
