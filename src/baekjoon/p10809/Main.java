7259b74f31c9fb0a496107f9a68ef9cdf3ee01b4
Sion <ygonepiece@naver.com>
2020-09-25 16:39:12 +0900

    알파벳 찾기(10809) 성공

---
+++ src/baekjoon/p10809/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.p10809;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+		sc.close();
+		int[] ch = new int[26];
+		Arrays.fill(ch, -1);
+		for (int i = 0; i < S.length(); i++) {
+			int pos = S.charAt(i) - 'a';
+			if (ch[pos] == -1) {ch[pos] = i;}
+		}
+		
+		for (int i = 0; i < ch.length; i++) {
+			System.out.print(ch[i]);
+			if(i != ch.length - 1) {System.out.print(" ");}
+		}
+	}
+}
