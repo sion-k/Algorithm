36cb3e5fcda349d1813664c1ad1bbe62f8306d2c
Sion <ygonepiece@naver.com>
2020-09-25 17:29:03 +0900

    계단 오르기(2579) 성공

--- src/baekjoon/p10809/Main.java
+++ src/baekjoon/p10809/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String S = sc.nextLine();
