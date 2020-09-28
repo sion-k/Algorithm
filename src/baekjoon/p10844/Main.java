5cbc00c8527b1dcb978f8df23f76318c15a7d9ae
Sion <ygonepiece@naver.com>
2020-09-28 16:26:14 +0900

    쉬운 계단 수(10844) 성공

--- src/baekjoon/p10844/Main.java
+++ src/baekjoon/p10844/Main.java
@@ -3,20 +3,31 @@ package baekjoon.p10844;
 import java.util.Scanner;
 
 public class Main {
-	static int[] cache;
+	static int[][] cache;
+	static int N;
 	
-	static int stair(int N) {
-		if (N == 1) {return 9;}
-		if (cache[N] != 0) {return cache[N];}
-		return cache[N] = (2 * stair(N - 1) - (N - 1)) % 1_000_000_000 ;
+	// [1, N]번째 숫자를 d를 골랐을 때 다오는 계단 수 경우의 수
+	static int stair(int i, int d) {
+		// 기저 사례 : 마지막 글자는 뭘 고르건 한가지
+		if (i == N) {return 1;}
+		if (cache[i][d] != 0) {return cache[i][d];}
+		if (d == 0) {return cache[i][d] = stair(i + 1, 1);}
+		if (d == 9) {return cache[i][d] = stair(i + 1, 8);}
+		return cache[i][d] = (stair(i + 1, d - 1) + stair(i + 1, d + 1)) % 1000000000;
 	}
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
+		N = sc.nextInt();
 		sc.close();
-		cache = new int[N + 1];
-		System.out.println(stair(N));
+		cache = new int[N + 1][9 + 1];
+		
+		int stair = 0;
+		for (int i = 1; i <= 9; i++) {
+			stair = ((stair + stair(1, i)) % 1000000000);
+		}
+		
+		System.out.println(stair);
 	}
 
 }
