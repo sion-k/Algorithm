a50def905c16b4d24035a5ccc07a807e3ceb97b3
Sion <ygonepiece@naver.com>
2020-10-02 15:32:02 +0900

    그룹 단어 체커(1316) 성공

---
+++ src/baekjoon/p1316/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.p1316;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static boolean groupWord(String word) {
+		boolean[] ch = new boolean[26];
+		int index = 0;
+		while(index < word.length()) {
+			if(ch[word.charAt(index) - 'a']) {return false;}
+			ch[word.charAt(index) - 'a'] = true;
+			int temp = index;
+			while(index < word.length() && word.charAt(temp) == word.charAt(index)) {
+				index++;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = Integer.parseInt(sc.nextLine());
+		int groupWord = 0;
+		for (int i = 0; i < N; i++) {
+			if(groupWord(sc.nextLine())) {groupWord++;}
+		}
+		sc.close();
+		System.out.println(groupWord);
+	}
+
+}
