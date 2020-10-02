0c5b7b86aaee6d6215027742357b906ab07d1112
Sion <ygonepiece@naver.com>
2020-10-02 13:52:37 +0900

    크게 만들기(2812) 시간 초과

--- src/baekjoon/p2812/Main.java
+++ src/baekjoon/p2812/Main.java
@@ -6,26 +6,37 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		char[] temp = br.readLine().toCharArray();
+		//StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		//int N = Integer.parseInt(st.nextToken());
+		int N = new Random().nextInt(500000) + 1;
+		System.out.println(N);
+		//int K = Integer.parseInt(st.nextToken());
+		int K = new Random().nextInt(N - 1) + 1;
+		System.out.println(K);
+		//char[] temp = br.readLine().toCharArray();
 		br.close();
 		LinkedList<Integer> S = new LinkedList<>();
-		for(int i = 0; i < N; i++) {S.add(temp[i] - '0');}
+		S.add(new Random().nextInt(9) + 1);
+		for(int i = 1; i < N; i++) {
+			//S.add(temp[i] - '0');
+			S.add(new Random().nextInt(10));
+		}
 		
-		for (int i = 0; i < S.size() - 1 && K > 0; i++) {
-			if (S.get(i) < S.get(i + 1)) {
-				S.remove(i);
-				i--;
+		int index = 0;
+		
+		while(index < S.size() - 1 && K > 0) {
+			if(S.get(index) < S.get(index + 1)) {
+				S.remove(index);
 				K--;
+				if (index != 0) {index--;}
+			} else {
+				index++;
 			}
 		}
 		
