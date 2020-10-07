95cc2e5551cd2a82bd19f1feab4989dd6a287752
Sion <ygonepiece@naver.com>
2020-10-07 16:29:01 +0900

    소트인사이드(1427) 성공

---
+++ src/baekjoon/p01427/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.p01427;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] N = sc.nextLine().toCharArray();
+		sc.close();
+		Arrays.sort(N);
+		for (int i = N.length - 1; i >=0; i--) {
+			System.out.print(N[i]);
+		}
+		System.out.println();
+	}
+
+}
