64cbb401df520f8fc2e05859bcf996c673aec406
Sion <ygonepiece@naver.com>
2020-10-08 19:32:53 +0900

    영역 구하기(2583) 성공

--- src/baekjoon/p02583/Main.java
+++ src/baekjoon/p02583/Main.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
