2215e674c7076329caa6261acfede9edaa39bc6f
Sion <ygonepiece@naver.com>
2020-10-09 20:22:14 +0900

    별 찍기 - 4(2441) 성공

--- src/baekjoon/p02440/Main.java
+++ src/baekjoon/p02440/Main.java
@@ -8,6 +8,9 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt(); sc.close();
 		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print(" ");
+			}
 			for (int j = 0; j < N - i; j++) {
 				System.out.print("*");
 			}
