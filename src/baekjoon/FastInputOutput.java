4e3b826d0b90bfddb3eb32c8778b567a44405689
Sion <ygonepiece@naver.com>
2020-10-10 22:22:45 +0900

    기타줄(1049) 성공

--- src/baekjoon/FastInputOutput.java
+++ src/baekjoon/FastInputOutput.java
@@ -15,11 +15,9 @@ public class FastInputOutput {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		st.nextToken();
+		Integer.parseInt(st.nextToken());
 		br.close();
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write("");
-		bw.newLine();
 		bw.close();
 	}
 
