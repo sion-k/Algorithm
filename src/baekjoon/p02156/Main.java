0bb8c4323e6ea62163c4a299ed8acd950427c2c4
Sion <ygonepiece@naver.com>
2020-10-11 14:47:10 +0900

    포도주 시식(2156) 성공

--- src/baekjoon/p02156/Main.java
+++ src/baekjoon/p02156/Main.java
@@ -10,38 +10,54 @@ public class Main {
 	static int[] W;
 	static int[][] cache;
 	
-	// i번째 잔을 마셨을 때 나머지 잔을 마셔서 얻을 수 있는 최대값
-	// 0 : 둘다 마심 1 : 첫 번째 잔만 마심 2 : 둘다 안 마심
-	static int dp(int i, int prev) {
+	// 이전에 prev 선택을 했을 때 i번째부터 선택해서 얻을 수 있는 최대 값
+	static int dp(int prev, int i) {
 		if (i >= N) {return 0;}
-		if (i  == N - 1 && prev != 0) {return W[i];}
-		if (cache[i][prev] != -1) { return cache[i][prev];}
-		if (prev == 0) {
-			return cache[i][prev] = dp(i + 1, 1);
+		if (i == N - 1 && prev != 0) {return W[i];}
+		if (cache[prev][i] != -1) {return cache[prev][i];}
+		int ret = 0;
+		switch (prev) {
+		case 0:// OO
+			ret = dp(1, i + 1);
+			break;
+		case 1:// OX
+			ret = Math.max(W[i] + dp(2, i + 1), W[i + 1] + dp(2, i + 2));
+			break;
+		case 2:// XO
+			ret = Math.max(W[i] + dp(1, i + 2), dp(1, i + 1));
+			break;
+		case 3:// XX
+			ret = W[i] + dp(2, i + 1);
+			break;
 		}
-		if (prev == 1) {
-			return cache[i][prev] = 
-			W[i] + Math.max(W[i + 1] + dp(i + 2,  0), dp(i + 2,  1));
-		}
-		if (prev == 2) {
-			return cache[i][prev] = 
-			W[i] + Math.max(W[i + 1] + dp(i + 3, 1), dp(i + 2, 1));
-		}
-		return -1;
+		return cache[prev][i] = ret;
 	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		W = new int[N];
-		cache = new int[N][3];
-		for (int i = 0; i < N; i++) {
+		cache = new int[4][N];
+		for (int i = 0; i < 4; i++) {
 			Arrays.fill(cache[i], -1);
 		}
 		for (int i = 0; i < N; i++) {W[i] = Integer.parseInt(br.readLine());}
-		
+		int max = 0;
+		if (N >= 3) {
+			int[] c = new int[4];
+			c[0] = dp(0, 2) + W[0] + W[1];
+			c[1] = dp(1, 2) + W[0];
+			c[2] = dp(2, 2) + W[1];
+			c[3] = dp(3, 2);
+			Arrays.sort(c);
+			max = c[3];
+		} else if (N == 2) {
+			max = W[0] + W[1];
+		} else if (N == 1) {
+			max = W[0];
+		}
+		System.out.println(max);
 		br.close();
-		System.out.println(dp(0, 2));
 	}
 
 }
