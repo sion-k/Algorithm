8f282f8644494a305b90597b130a1112a910a40a
Sion <ygonepiece@naver.com>
2020-10-11 14:02:46 +0900

    단어의 개수(1152) 성공

--- src/baekjoon/p11650/Main.java
+++ src/baekjoon/p11650/Main.java
@@ -5,9 +5,8 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Collections;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
