14eb7b14a372a76e5e133b3478f0c56b382a2bb9
Sion <ygonepiece@naver.com>
2020-10-12 19:34:02 +0900

    숫자카드(10815) 성공

--- src/baekjoon/p02869/Main.java
+++ src/baekjoon/p02869/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		int B = sc.nextInt();
 		int V = sc.nextInt();
 		sc.close();
+		// (V - A)까지 올라가려면 며칠이 걸리는지
 		int days = (V - A) / (A - B);
 		int lifted = days * (A - B);
 		if (V - lifted > A) {days++;}
