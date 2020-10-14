b02a6eefdfbc50a9086b24b77040a95ec929eb4e
Sion <ygonepiece@naver.com>
2020-10-14 17:16:39 +0900

    열 개씩 끊어 출력하기(11721) 성공

---
+++ src/baekjoon/p11721/Main.java
@@ -0,0 +1,18 @@
+package baekjoon.p11721;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+		sc.close();
+		for (int i = 0, j = 0; i < line.length(); i++, j++) {
+			if (j == 10) {System.out.println(); j = 0;}
+			System.out.print(line.charAt(i));
+		}
+
+	}
+
+}
