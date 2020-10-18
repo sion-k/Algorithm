14c52fc0353730cf306f75312886e9ee9bcdb4a7
Sion <ygonepiece@naver.com>
2020-10-18 12:13:56 +0900

    동물원(1309) 성공

---
+++ src/baekjoon/p01309/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.p01309;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static int N;
+	static int[][] cache;
+
+	static int dp(int i, int prev) {
+		if (i >= N) {return 1;}
+		if (cache[i][prev] != 0) {return cache[i][prev];}
+		cache[i][prev] = (cache[i][prev] + dp(i + 1, 0)) % 9901;
+		cache[i][prev] = (cache[i][prev] + dp(i + 1, 1)) % 9901;
+		if (prev == 0) {
+			cache[i][prev] = (cache[i][prev] + dp(i + 1, 1)) % 9901;
+		}
+		return cache[i][prev];
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		cache = new int[N][2];
+		System.out.println(dp(0, 0));
+	}
+
+}
