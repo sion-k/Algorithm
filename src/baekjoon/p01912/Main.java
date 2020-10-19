1651d4e7289ec51ed68b0da0ba74f75081fefa7c
Sion <ygonepiece@naver.com>
2020-10-19 22:14:36 +0900

    서강그라운드(14938) 성공

--- src/baekjoon/p01912/Main.java
+++ src/baekjoon/p01912/Main.java
@@ -3,36 +3,28 @@ package baekjoon.p01912;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-/*
- * 빠른 입 출력, 문자열 처리
- */
 public class Main {
+	static int N;
 	static int[] S;
-	static ArrayList<Integer> cache;
-	
+	static int[] cache;
+
+	// i부터 선택할 수 있는 연속합중 최대
+	static int dp(int i) {
+		if (i >= N) {return 0;}
+		if (cache[i] != -10000001) {return cache[i];}
+		return cache[i] = Math.max(dp(i + 1), S[i] + dp(i + 1));
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		S = new int[N];
-		cache = new ArrayList<>(N);
+		N = Integer.parseInt(br.readLine());
+		S = new int[N]; cache = new int[N]; Arrays.fill(cache, -10000001);
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int sum = 0;
-		// cache[i] = [0, i] 까지의 부분 합
-		// 연속합[i, j] = cache[j] - cache[i - 1];
-		for(int i = 0 ; i < N; i++) {
-			S[i] = Integer.parseInt(st.nextToken());
-			sum += S[i];
-			cache.add(sum);
-		}
-		br.close();
-		System.out.println(cache);
-		Collections.sort(cache);
-		System.out.println(cache);
-		System.out.println(cache.get(N - 1) - cache.get(0));
+		for(int i = 0 ; i < N; i++) {S[i] = Integer.parseInt(st.nextToken());}
+		System.out.println(dp(0));
 	}
 
 }
