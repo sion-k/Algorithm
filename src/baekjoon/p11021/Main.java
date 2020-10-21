d82c062903d6ca5131ac5e5f6e882c3f2115c32a
Sion <ygonepiece@naver.com>
2020-10-21 20:20:53 +0900

    A+B - 7(11021) 성공

--- src/baekjoon/p11021/Main.java
+++ src/baekjoon/p11021/Main.java
@@ -1,10 +1,26 @@
 package baekjoon.p11021;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		for (int c = 1; c <= T; c++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			bw.write("Case #" + c + ": " + String.valueOf(A + B));
+			bw.newLine();
+		}
+		bw.close();
 	}
 
 }
