7d4a38778b5de8aa4676a6d554e4e8aa7fc43fb4
Sion <ygonepiece@naver.com>
2020-10-27 19:12:11 +0900

    퇴사(14501) 성공

--- src/baekjoon/p14501/Main.java
+++ src/baekjoon/p14501/Main.java
@@ -1,31 +1,39 @@
 package baekjoon.p14501;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int[][] C; // 시간과 금액
+	static int[] T; static int[] P;
 	static int[] cache;
 
+	// i번째 일 이후로 얻을 수 있는 최대 이득
+	static int dp(int i) {
+		if (i > N) {return 0;}
+		if (cache[i] != -1) {return cache[i];}
+		cache[i] = dp(i + 1);
+		// 상담이 끝나는 날짜가 퇴사 전이라면
+		if (i + T[i] - 1 <= N) {
+			cache[i] = Math.max(cache[i], P[i] + dp(i + T[i]));
+		}
+		return cache[i];
+	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		C = new int[N][2];
-		for (int i = 0; i < N; i++) {
+		cache = new int[N + 1]; Arrays.fill(cache, -1);
+		T = new int[N + 1]; P = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			C[i][0] = Integer.parseInt(st.nextToken());
-			C[i][1] = Integer.parseInt(st.nextToken());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
 		}
-		br.close();
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.close();
+		System.out.println(dp(1));
 	}
-
 
 }
