1af680d998bd0c9a63dca214489f6c05476dfa5c
Sion <ygonepiece@naver.com>
2020-10-27 18:58:53 +0900

    연속합(1912) 성공

--- src/baekjoon/p01912/Main.java
+++ src/baekjoon/p01912/Main.java
@@ -7,24 +7,30 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
+	static final int INF = 987654321;
 	static int[] S;
-	static int[] cache;
+	static int[][] cache;
 
-	// i부터 선택할 수 있는 연속합중 최대
-	static int dp(int i) {
-		if (i >= N) {return 0;}
-		if (cache[i] != -10000001) {return cache[i];}
-		return cache[i] = Math.max(dp(i + 1), S[i] + dp(i + 1));
+	// i번째 까지의 연속합중 i번째 연속합을 선택하거나 선택하지 않는 합 중 최대
+	static int dp(int i, int c) {
+		if (i < 0) {return 0;}
+		if (cache[c][i] != INF) {return cache[c][i];}
+		if (c == 0) {
+			return cache[c][i] = Math.max(dp(i - 1, 0), dp(i - 1, 1));
+		} else {
+			return cache[c][i] = S[i] + Math.max(0, dp(i - 1, 1));
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		S = new int[N]; cache = new int[N]; Arrays.fill(cache, -10000001);
+		int N = Integer.parseInt(br.readLine());
+		S = new int[N]; cache = new int[2][N];
+		Arrays.fill(cache[0], INF); Arrays.fill(cache[1], INF);
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for(int i = 0 ; i < N; i++) {S[i] = Integer.parseInt(st.nextToken());}
-		System.out.println(dp(0));
+		cache[0][0] = S[0];
+		System.out.println(Math.max(dp(N - 1, 0), dp(N - 1, 1)));
 	}
 
 }
--- src/baekjoon/p02884/Main.java
+++ src/baekjoon/p02884/Main.java
@@ -21,4 +21,5 @@ public class Main {
 		bw.write(H + " " + M);
 		bw.close();
 	}
+
 }
