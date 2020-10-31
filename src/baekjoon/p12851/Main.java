2ed49bba342d70a51cdea53e411d63671c16ac42
Sion <ygonepiece@naver.com>
2020-10-31 16:42:08 +0900

    숨바꼭질 2(12851) 2트

--- src/baekjoon/p12851/Main.java
+++ src/baekjoon/p12851/Main.java
@@ -3,46 +3,52 @@ package baekjoon.p12851;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N; static int K;
-	static int minPath;
+	static int[] WAY = new int[100001];
+
 	static boolean inRange(int here) {return 0 <= here && here <= 100000;}
 
+	static int move(int here, int moveWay) {
+		switch(moveWay) {
+		case 0:
+			return here - 1;
+		case 1:
+			return here + 1;
+		default:
+			return 2 * here;
+		}
+	}
+
 	static int BFS(int start) {
-		if (start == K) {return 0;}
 		Queue<Integer> q = new LinkedList<>();
 		q.offer(start);
-		boolean[] BOOKED = new boolean[100001];
-		BOOKED[start] = true;
-		int[] DIST = new int[100001];
-		DIST[start] = 0;
+		boolean[] visit = new boolean[100001];
+		visit[start] = true;
+		int[] dist = new int[100001];
+		Arrays.fill(dist, -1);
+		dist[start] = 0;
+		WAY[start] = 1;
+		if (start == K) {return 0;}
 
 		while (!q.isEmpty()) {
 			int here = q.poll();
-			int left = here - 1;
-			if (inRange(left) && !BOOKED[left]) {
-				q.offer(left);
-				BOOKED[left] = true;
-				DIST[left] = DIST[here] + 1;
-				if (left == K) {return DIST[left];}
-			}
-			int right = here + 1;
-			if (inRange(right) && !BOOKED[right]) {
-				q.offer(right);
-				BOOKED[right] = true;
-				DIST[right] = DIST[here] + 1;
-				if (right == K) {return DIST[right];}
-			}
-			int warp = here * 2;
-			if (inRange(warp) && !BOOKED[warp]) {
-				q.offer(warp);
-				BOOKED[warp] = true;
-				DIST[warp] = DIST[here] + 1;
-				if (warp == K) {return DIST[warp];}
+			System.out.println("here : " + here);
+			if (here == K) {return dist[here];}
+			visit[here] = true;
+			for (int m = 0; m < 3; m++) {
+				int there = move(here, m);
+				System.out.println("there : " + there);
+				if (inRange(there) && !visit[there]) {
+					q.offer(there);
+					if (dist[there] == -1) {dist[there] = dist[here] + 1;}
+					if (dist[there] == dist[here] + 1) {WAY[there] += WAY[here];}
+				}
 			}
 		}
 		return -1;
@@ -53,7 +59,8 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		System.out.println(minPath = BFS(N));
+		System.out.println(BFS(N));
+		System.out.println(WAY[K]);
 	}
 
 }
