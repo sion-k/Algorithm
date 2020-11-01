963576a6ca50d93eb75f7f723ce627f407c8ed95
Sion <ygonepiece@naver.com>
2020-11-01 17:46:06 +0900

    구간 합 구하기 5(11660) 성공

--- src/baekjoon/p11660/Main.java
+++ src/baekjoon/p11660/Main.java
@@ -24,10 +24,9 @@ public class Main {
 		pSum = new int[N + 1][N + 1];
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
-			int rowSum = 0;
 			for (int j = 1; j <= N; j++) {
-				rowSum += Integer.parseInt(st.nextToken());
-				pSum[i][j] = rowSum + pSum[i - 1][j];
+				pSum[i][j] = pSum[i][j - 1] + pSum[i - 1][j] - pSum[i - 1][j - 1] +
+				Integer.parseInt(st.nextToken());
 			}
 		}
 		for (int i = 0; i < M; i++) {
