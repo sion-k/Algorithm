59baa664e7168da1942a008335cf52aa04ecc5e3
Sion <ygonepiece@naver.com>
2020-11-05 19:29:41 +0900

    A → B(16953) 성공

--- src/baekjoon/p16953/Main.java
+++ src/baekjoon/p16953/Main.java
@@ -3,18 +3,35 @@ package baekjoon.p16953;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static long move(int here, int moveWay) {
+		if (moveWay == 0) {return 2 * here;}
+		else {return 10 * here + 1;}
+	}
 
-	static int BTK(int A, int B) {
-		if (A == B) {return 0;}
-		int min = 32;
-		int mult = A * 2;
-		if (mult <= B) {min = Math.min(min, BTK(mult, B));}
-		long extend = A * 10 + 1;
-		if (extend <= B) {min = Math.min(min, BTK((int)extend, B));}
-		return 1 + min;
+	static int BFS(int A, int B) {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(A);
+		Map<Integer, Integer> dist = new HashMap<>();
+		dist.put(A, 1);
+		while (!q.isEmpty()) {
+			int here = q.poll();
+			for (int d = 0; d < 2; d++) {
+				long there = move(here, d);
+				if (there <= B && !dist.containsKey((int)there)) {
+					dist.put((int)there, dist.get(here) + 1);
+					q.offer((int)there);
+					if (there == B) {return dist.get((int)there);}
+				}
+			}
+		}
+		return -1;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -22,9 +39,7 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
-		int ret = BTK(A, B) + 1;
-		if (ret >= 32) {ret = -1;}
-		System.out.println(ret);
+		System.out.println(BFS(A, B));
 	}
 
 }
