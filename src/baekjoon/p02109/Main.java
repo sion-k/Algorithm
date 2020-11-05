b83f55db4b88953b60d5b293cd094638293e51f6
Sion <ygonepiece@naver.com>
2020-11-05 18:52:47 +0900

    순회강연(2109) 실패

--- src/baekjoon/p02109/Main.java
+++ src/baekjoon/p02109/Main.java
@@ -1,54 +1,40 @@
 package baekjoon.p02109;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	private static class Lecture implements Comparable<Lecture> {
-		public int p;
-		public int d;
-
-		public Lecture(int p, int d) {this.p = p;this.d = d;}
-
-		@Override
-		public int compareTo(Lecture o) {
-			return d == o.d ? o.p - p : o.d - d ;
-		}
-
-		@Override
-		public String toString() {
-			return p + " " + d;
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(br.readLine());
-		Queue<Lecture> queue = new PriorityQueue<>();
-		StringTokenizer st = null;
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			queue.offer(new Lecture(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Lecture> pq = new PriorityQueue<>(N);
+		boolean[] taken = new boolean[10001];
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int price = Integer.parseInt(st.nextToken());
+			int date = Integer.parseInt(st.nextToken());
+			pq.offer(new Lecture(price, date));
 		}
-		System.out.println(queue);
 		int sum = 0;
-		int date = 10000;
-		boolean[] booked = new boolean[10000 + 1];
-		while (!queue.isEmpty() && date >= 1) {
-			if(booked[date]) {date--; continue;}
-			// 마감 기한이 같은 일 중에서 가장 비싼걸 뽑는다
-			Lecture l = queue.peek();
-			// 현재 날짜가 뽑은 강연의 기한 내라면
-			if (date <= l.d) {
-				l = queue.poll();
-				System.out.println(date + "날에" + l + "예약");
-				sum += l.p;
-				booked[date] = true;
-			}
-			date--;
+		while (!pq.isEmpty()) {
+			Lecture lec = pq.poll(); int date = lec.date;
+			while (date >= 1 && taken[date]) {date--;}
+			if (date >= 1) {taken[date]=  true; sum += lec.price;}
 		}
 		System.out.println(sum);
 	}
+
+}
+
+class Lecture implements Comparable<Lecture> {
+	int price; int date;
+	public Lecture(int p, int d) {price = p; date = d;}
+	@Override
+	public int compareTo(Lecture o) {return o.price - price;}
 
 }
