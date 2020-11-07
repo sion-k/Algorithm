ef3cf8d6d7e0049ccd5371fe7432ba709c6571c2
Sion <ygonepiece@naver.com>
2020-11-07 20:05:47 +0900

    문제집(1766) 성공

--- src/baekjoon/p01766/Main.java
+++ src/baekjoon/p01766/Main.java
@@ -4,55 +4,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static boolean[] visit;
-	static ArrayList<PriorityQueue<Integer>> adj;
-
-	static ArrayList<Integer> sorted = new ArrayList<>();
+	static int[] indegree;
 	static StringBuilder ans = new StringBuilder();
 
-	static void DFS(int here) {
-		visit[here] = true;
-		while (!adj.get(here).isEmpty()) {
-			int there = adj.get(here).poll();
-			if (!visit[there]) {DFS(there);}
-		}
-		sorted.add(here);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        indegree = new int[N + 1];
 
-	static void topologicalSort() {
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) {DFS(i);}
-		}
-		for (int i = 0; i < sorted.size(); i++) {
-			ans.append(sorted.get(i)); ans.append(" ");
-		}
-		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) {ans.append(i); ans.append(" ");}
-		}
-	}
+        List<List<Integer>> list = new ArrayList<List<Integer>>();
+        list.add(new ArrayList<>());
+		for (int i = 1; i <= N; i++) {list.add(new ArrayList<>());}
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		visit = new boolean[N + 1];
-		adj = new ArrayList<>(N);
-		adj.add(new PriorityQueue<>());
-		for (int i = 0; i < N; i++) {adj.add(new PriorityQueue<>());}
-		int M = Integer.parseInt(st.nextToken());
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int end = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
 			int start = Integer.parseInt(st.nextToken());
-			adj.get(start).offer(end);
+			int end = Integer.parseInt(st.nextToken());
+			list.get(start).add(end);
+			indegree[end]++;
 		}
-		topologicalSort();
-		System.out.println(ans.toString().trim());
-	}
+        topologicalSort(list);
+        System.out.println(ans.toString().trim());
+    }
+
+    static void topologicalSort(List<List<Integer>> list) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		for (int i = 1; i <= N; i++) {if (indegree[i] == 0) {pq.offer(i);}}
+
+        while(!pq.isEmpty()) {
+            int here = pq.poll();
+            for(Integer there : list.get(here)) {
+                indegree[there]--;
+                if(indegree[there] == 0) {pq.offer(there);}
+            }
+            ans.append(here); ans.append(" ");
+        }
+    }
 
 }
