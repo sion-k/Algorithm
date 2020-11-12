d44b120119d43033d313fa7c508a10758346e049
Sion <ygonepiece@naver.com>
2020-11-13 07:35:33 +0900

    체스판 다시 칠하기(1018) 성공

--- src/baekjoon/p01018/Main.java
+++ src/baekjoon/p01018/Main.java
@@ -1,20 +1,58 @@
 package baekjoon.p01018;
 
-public class Main {
-	// false는 검은색 true는 흰색
-	static boolean[][] BOARD;
-	
-//	// 체스판은 2가지 경우 맨왼쪽이 흰색이거나 검은색
-//	static int paint(int y, int x) {
-//		for(int i = y; i < y + 8; i++) {
-//			for (int j = x; j < x + 8; j++) {
-//				
-//			}
-//		}
-//	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Main {
+	// true 흰색 false 검은색
+	static boolean[][] BOARD;
+
+	// 체스판은 2가지 경우 맨왼쪽이 흰색이거나 검은색
+	static int paint(int y, int x) {
+		int whiteBoard = 0;
+		for (int i = y; i < y + 8; i++) {
+			for (int j = x; j < x + 8; j++) {
+				if (i % 2 == j % 2) {
+					whiteBoard += BOARD[i][j] ? 0 : 1;
+				} else {
+					whiteBoard += BOARD[i][j] ? 1 : 0;
+				}
+			}
+		}
+		int blackBoard = 0;
+		for (int i = y; i < y + 8; i++) {
+			for (int j = x; j < x + 8; j++) {
+				if (i % 2 == j % 2) {
+					blackBoard += !BOARD[i][j] ? 0 : 1;
+				} else {
+					blackBoard += !BOARD[i][j] ? 1 : 0;
+				}
+			}
+		}
+		return Math.min(whiteBoard, blackBoard);
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		BOARD = new boolean[N][M];
+		for (int i = 0; i < N; i++) {
+			String row = br.readLine();
+			for (int j = 0; j < M; j++) {
+				BOARD[i][j] = row.charAt(j) == 'W';
+			}
+		}
+		int min = N * M;
+		for (int i = 0; i <= N - 8; i++) {
+			for (int j = 0; j <= M - 8; j++) {
+				min = Math.min(min, paint(i, j));
+			}
+		}
+		System.out.println(min);
 	}
 
 }
