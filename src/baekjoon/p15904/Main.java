0d62796ce050d560226eb0285a1e5277f3bf9e53
Sion <ygonepiece@naver.com>
2020-11-15 02:12:32 +0900

    UCPC는 무엇의 약자일까?(15904) 성공

---
+++ src/baekjoon/p15904/Main.java
@@ -0,0 +1,19 @@
+package baekjoon.p15904;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		if (S.matches(".*U.*C.*P.*C.*")) {
+			System.out.println("I love UCPC");
+		} else {
+			System.out.println("I hate UCPC");
+		}
+	}
+
+}
