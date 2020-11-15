aeb23ea2ad13f87c25c644109e8529e2a6509aaf
Sion <ygonepiece@naver.com>
2020-11-15 13:49:37 +0900

    공통 부분 문자열(5582) 성공

--- src/baekjoon/TestCaseGenerator.java
+++ src/baekjoon/TestCaseGenerator.java
@@ -13,10 +13,13 @@ public class TestCaseGenerator {
 		BufferedReader br = new BufferedReader(new FileReader("testCase.txt"));
 		br.close();
 		BufferedWriter bw = new BufferedWriter(new FileWriter("testCase.txt"));
-		bw.write("1000000"); bw.newLine();
-		for (int w = 0; w < 1000000; w++) {
-			bw.write(String.valueOf(new Random().nextInt(1000000) + 1));
-			bw.write(" ");
+		//bw.write("1000000"); bw.newLine();
+		for (int w = 0; w < 4000; w++) {
+			bw.write(String.valueOf(new Random().nextInt(26) + 'A'));
+		}
+		bw.newLine();
+		for (int w = 0; w < 4000; w++) {
+			bw.write(String.valueOf(new Random().nextInt(26) + 'A'));
 		}
 		bw.newLine();
 		bw.close();
