67c07bbdd8e75cfc42e075c0d517b890125bef96
Sion <ygonepiece@naver.com>
2020-11-15 13:10:25 +0900

    풍선 맞추기(11509) 성공

--- src/baekjoon/TestCaseGenerator.java
+++ src/baekjoon/TestCaseGenerator.java
@@ -13,14 +13,12 @@ public class TestCaseGenerator {
 		BufferedReader br = new BufferedReader(new FileReader("testCase.txt"));
 		br.close();
 		BufferedWriter bw = new BufferedWriter(new FileWriter("testCase.txt"));
-		bw.write("1000"); bw.newLine();
-		bw.write("1000"); bw.newLine();
-		for (int w = 0; w < 1000; w++) {
-			bw.write(String.valueOf(new Random().nextInt(1000) + 1));
+		bw.write("1000000"); bw.newLine();
+		for (int w = 0; w < 1000000; w++) {
+			bw.write(String.valueOf(new Random().nextInt(1000000) + 1));
 			bw.write(" ");
-			bw.write(String.valueOf(new Random().nextInt(1000) + 1));
-			bw.newLine();
 		}
+		bw.newLine();
 		bw.close();
 	}
 
