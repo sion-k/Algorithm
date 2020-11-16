c52475d9585fcbd052a8a6b82f3416ff266213f0
Sion <ygonepiece@naver.com>
2020-11-16 20:17:47 +0900

    돌 게임 5(9659) 성공

--- src/baekjoon/p09659/Main.java
+++ src/baekjoon/p09659/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long N = Long.parseLong(br.readLine());
-		System.out.println(N % 2 == 0 ? "SK" : "CY");
+		System.out.println(N % 2 == 1 ? "SK" : "CY");
 	}
 
 }
