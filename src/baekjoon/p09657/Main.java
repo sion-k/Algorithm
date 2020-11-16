f798b17c8f45c5520e1ae9f44aada2b1c4fb5642
Sion <ygonepiece@naver.com>
2020-11-16 20:07:28 +0900

    돌 게임3(9657) 성공

---
+++ src/baekjoon/p09657/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.p09657;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	static int N; static int[] cache;
+
+	// i[1, N]번째에서 턴을 시작할 때 이길 수 있는지 여부
+	static int dp(int i) {
+		if (i == N + 1) {return 0;}
+		if (cache[i] != -1) {return cache[i];}
+		if (dp(i + 1) == 0 || (i + 3 <= N + 1 && dp(i + 3) == 0) ||
+				(i + 4 <= N + 1 && dp(i + 4) == 0)) {
+			return cache[i] = 1;
+		}
+		return cache[i] = 0;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		cache = new int[N + 1]; Arrays.fill(cache, -1);
+		System.out.println(dp(1) == 1 ? "SK" : "CY");
+	}
+
+}
