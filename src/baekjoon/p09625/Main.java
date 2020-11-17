03cba6cccaa836d9ec519b50b3bcb75105cca936
Sion <ygonepiece@naver.com>
2020-11-17 20:12:48 +0900

    BABBA(9625) 성공

---
+++ src/baekjoon/p09625/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.p09625;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int K = Integer.parseInt(br.readLine());
+		int[][] dp = new int[K + 1][2];
+		dp[0][0] = 1;
+		for (int i = 1; i <= K; i++) {
+			dp[i][0] = dp[i - 1][1];
+			dp[i][1] = dp[i - 1][0] + dp[i - 1][1];
+		}
+		System.out.println(dp[K][0] + " " + dp[K][1]);
+	}
+
+}
