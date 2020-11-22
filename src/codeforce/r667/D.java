60d5e3ee4ce092289279ec113afe03c2beaafd7d
Sion <ygonepiece@naver.com>
2020-11-22 19:35:35 +0900

    CodeForces(R667)

--- src/codeforce/r667/D.java
+++ src/codeforce/r667/D.java
@@ -5,7 +5,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class D {
@@ -16,22 +15,22 @@ public class D {
 		int T = Integer.parseInt(br.readLine());
 		for (int c = 0; c < T; c++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			char[] n = new char[19]; Arrays.fill(n, ' ');
+			int[] n = new int[19];
 			String str = st.nextToken();
 			for (int i = 0, j = 19 - str.length(); i < str.length(); i++, j++) {
-				n[j] = str.charAt(i);
+				n[j] = str.charAt(i) - '0';
 			}
 			int s = Integer.parseInt(st.nextToken());
 			int digit = 0;
-			for (int i = 0; i < n.length; i++) {digit += (n[i] - '0');}
+			for (int i = 0; i < n.length; i++) {digit += n[i];}
 			int p = 18;
-			while (n[p] != ' ' && digit > s) {
-				digit -= (n[p] - '0');
-				n[p] = '0'; p--;
-				if (n[p - 1] == ' ') {n[p - 1] = '1'; digit++;}
-				System.out.println(digit);
+			while (p >= 1 && n[p] != ' ' && digit > s) {
+				digit -= n[p]; n[p] = 0;
+				int t = p - 1;
+				while (n[t] == 9) {n[t] = 0; digit -= 9; t--;}
+				n[t]++; digit++;
+				p--;
 			}
-			System.out.println(Arrays.toString(n));
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < 19; i++) {sb.append(n[i]);}
 			long start = Long.parseLong(str);
