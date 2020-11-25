3195eb295fea8193a5969945052d5964fc9f3633
Sion <ygonepiece@naver.com>
2020-11-25 18:31:50 +0900

    1, 2, 3 더하기 3(15988) 성공

--- src/baekjoon/p15988/Main.java
+++ src/baekjoon/p15988/Main.java
@@ -5,31 +5,24 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
-	static int[] cache;
 	static final int MOD = 1000000009;
-
-	// N을 1, 2, 3의 합으로 나타내는 방법의 수 반환
-	static int dp(int N) {
-		if (N < 0) {return 0;}
-		if (N == 0) {return 1;}
-		if (cache[N] != -1) {return cache[N];}
-		int sum = 0;
-		for (int i = 1; i <= 3; i++) {sum = (sum + dp(N - i)) % MOD;}
-		return cache[N] = sum;
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		cache = new int[1000001];
-		Arrays.fill(cache, -1);
+		int[] cache = new int[1000001];
+		cache[1] = 1; cache[2] = 2; cache[3] = 4;
+		for (int i = 4; i <= 1000000; i++) {
+			cache[i] = (cache[i] + cache[i - 1]) % MOD;
+			cache[i] = (cache[i] + cache[i - 2]) % MOD;
+			cache[i] = (cache[i] + cache[i - 3]) % MOD;
+		}
 		int T = Integer.parseInt(br.readLine());
 		for (int c = 0; c < T; c++) {
 			int N = Integer.parseInt(br.readLine());
-			bw.write(String.valueOf(dp(N)));
+			bw.write(String.valueOf(cache[N]));
 			bw.newLine();
 		}
 		bw.close();
