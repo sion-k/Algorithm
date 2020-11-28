ea240ae3fd8e515810a0e870d8f143b1e02c7790
Sion <ygonepiece@naver.com>
2020-11-28 20:37:21 +0900

    별 찍기 - 8(2445) 성공

--- src/baekjoon/p02446/Main.java
+++ src/baekjoon/p02446/Main.java
@@ -12,14 +12,18 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = 0; j < i; j++) {bw.write(" ");}
-			for (int j = 0; j < (2 * N - 1) - 2 * i; j++) {bw.write("*");}
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < i; j++) {bw.write("*");}
+			for (int j = 0; j <= (2 * N - 1) - 2 * i; j++) {bw.write(" ");}
+			for (int j = 0; j < i; j++) {bw.write("*");}
 			bw.newLine();
 		}
+		for (int j = 0; j < 2 * N; j++) {bw.write("*");}
+		bw.newLine();
 		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j < N - i; j++) {bw.write(" ");}
-			for (int j = 0; j < 2 * i - 1; j++) {bw.write("*");}
+			for (int j = 0; j < N - i; j++) {bw.write("*");}
+			for (int j = 0; j <= 2 * i - 1; j++) {bw.write(" ");}
+			for (int j = 0; j < N - i; j++) {bw.write("*");}
 			bw.newLine();
 		}
 		bw.close();
