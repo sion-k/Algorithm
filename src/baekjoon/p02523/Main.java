70fc4202ba83cce4f38f3f1230d1b3aa180ebf93
Sion <ygonepiece@naver.com>
2020-11-28 20:39:41 +0900

    별 찍기 - 13(2523) 성공

---
+++ src/baekjoon/p02523/Main.java
@@ -0,0 +1,26 @@
+package baekjoon.p02523;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			for (int j = 0; j < i; j++) {bw.write("*");}
+			bw.newLine();
+		}
+		for (int i = N - 1; i >= 1; i--) {
+			for (int j = 0; j < i; j++) {bw.write("*");}
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+}
