836630182aaaf562251b5075eff384872480f13a
Sion <ygonepiece@naver.com>
2020-11-29 20:38:09 +0900

    돌다리 건너기(2602)

--- src/baekjoon/p02602/Main.java
+++ src/baekjoon/p02602/Main.java
@@ -1,10 +1,24 @@
 package baekjoon.p02602;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+	static char[] S;
+	static char[] A; static char[] B;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// (i, j, k)다리를 건너가는 방법의 수 반환
+	static int dp(int i, int j, int k) {
+		// 끝에 도착하면 방법을 하나 찾음
+		if (i >= A.length) {return 1;}
 
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		S = br.readLine().toCharArray();
+		A = br.readLine().toCharArray();
+		B = br.readLine().toCharArray();
 	}
 
 }
