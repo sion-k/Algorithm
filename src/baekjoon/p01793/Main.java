bfa4f686f26cd6d4250b35cdc069b22d4bf309d7
Sion <ygonepiece@naver.com>
2020-11-30 08:24:54 +0900

    타일링(1793) 성공

---
+++ src/baekjoon/p01793/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.p01793;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BigInteger[] dp = new BigInteger[251];
+		dp[0] = BigInteger.ONE; dp[1] = BigInteger.ONE;
+		BigInteger two = new BigInteger("2");
+		dp[2] = new BigInteger("3");
+		for (int i = 3; i <= 250; i++) {
+			dp[i] = dp[i - 1].add(dp[i - 2].multiply(two));
+		}
+		String line = "";
+		while (!(line = br.readLine()).equals(" ")) {
+			int N = Integer.parseInt(line);
+			bw.write(String.valueOf(dp[N]));
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+}
