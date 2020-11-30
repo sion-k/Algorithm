7d8b202a421bbb35c0a2b156cbd078c076278175
Sion <ygonepiece@naver.com>
2020-11-30 17:02:33 +0900

    사회망 서비스(SNS)(2533) 성공

--- src/baekjoon/TestCaseGenerator.java
+++ src/baekjoon/TestCaseGenerator.java
@@ -12,31 +12,10 @@ public class TestCaseGenerator {
 		BufferedReader br = new BufferedReader(new FileReader("testCase.txt"));
 		br.close();
 		BufferedWriter bw = new BufferedWriter(new FileWriter("testCase.txt"));
-		int N = 100;
-		int M = 100;
-		int L = 10;
-		int G = 30000;
-		int INF = 1000;
-		int T = 10;
-		bw.write(String.valueOf(T)); bw.newLine();
-		for (int c = 0; c < T; c++) {
-			bw.write(N + " " + M + " " + L + " " + G); bw.newLine();
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M - 1; j++) {
-					int rand = (int)(Math.random() * INF) + 1;
-					bw.write(String.valueOf(rand));
-					if (j != M - 2) {bw.write(" ");}
-				}
-				bw.newLine();
-			}
-			for (int i = 0; i < N - 1; i++) {
-				for (int j = 0; j < M; j++) {
-					int rand = (int)(Math.random() * INF) + 1;
-					bw.write(String.valueOf(rand));
-					if (j != M - 1) {bw.write(" ");}
-				}
-				bw.newLine();
-			}
+		int N = 1000;
+		bw.write(String.valueOf(N)); bw.newLine();
+		for (int i = 1; i <= N - 1; i++) {
+			bw.write(i + " " + (i + 1)); bw.newLine();
 		}
 		bw.close();
 	}
