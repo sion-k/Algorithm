73b72a01ca2378afa23a91f82fb6613863b90aa3
Sion <ygonepiece@naver.com>
2020-12-07 07:18:01 +0900

    환상의 듀엣(11570) 구현 실패

--- src/baekjoon/p11570/Main.java
+++ src/baekjoon/p11570/Main.java
@@ -26,26 +26,20 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine()); S = new int[N];
+		N = Integer.parseInt(br.readLine());
+		S = new int[N];
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++)
 			S[i] = Integer.parseInt(st.nextToken());
 		cache = new int[N][N];
 		for (int i = 0; i < N; i++)
 			Arrays.fill(cache[i], -1);
-		int min = Integer.MAX_VALUE;
+		int min = dp(0, 1);
 		int cost = 0;
-		min = Math.min(min, dp(0, 1));
-		for (int j = 2; j < N; j++) {
-			cost += Math.abs(S[j] - S[j - 1]);
-			System.out.println(cost);
-			min = Math.min(min, cost + dp(0, j));
-		}
-		cost = 0;
-		min = Math.min(min, dp(1, 0));
 		for (int i = 2; i < N; i++) {
 			cost += Math.abs(S[i] - S[i - 1]);
-			min = Math.min(min, cost + dp(i, 0));
+			System.out.println(cost);
+			min = Math.min(min, cost + dp(0, i));
 		}
 		System.out.println(min);
 	}
