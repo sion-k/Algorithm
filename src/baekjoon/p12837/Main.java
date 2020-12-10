11b9c2d8d6b51ac8f453adc0d6139c2f36819fae
Sion <ygonepiece@naver.com>
2020-12-10 20:26:40 +0900

    가계부 (HARD) 실패

---
+++ src/baekjoon/p12837/Main.java
@@ -0,0 +1,60 @@
+package baekjoon.p12837;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		FenwickTree t = new FenwickTree(N);
+		int[] S = new int[N];
+		int Q = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < Q; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int com = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			int q = Integer.parseInt(st.nextToken());
+			if (com == 1) {
+				int diff = q - S[p - 1];
+				t.add(p - 1, diff);
+				S[p - 1] += diff;
+			} else {
+				long diff = t.sum(q - 1) - t.sum(p - 1);
+				bw.write(String.valueOf(diff));
+				bw.newLine();
+			}
+		}
+		bw.close();
+	}
+
+}
+class FenwickTree {
+	long[] tree;
+	public FenwickTree(int n) {tree = new long[n + 1];}
+
+	void add(int pos, int val) {
+		pos++;
+		while (pos < tree.length) {
+			tree[pos] += val;
+			pos += (pos & -pos);
+		}
+	}
+
+	long sum(int pos) {
+		pos++;
+		long ret = 0;
+		while (pos > 0) {
+			ret += tree[pos];
+			pos &= (pos - 1);
+		}
+		return ret;
+	}
+}
