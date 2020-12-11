d8f1d765e6dfe441e58fc967d533578c1702fdc8
Sion <ygonepiece@naver.com>
2020-12-11 18:42:24 +0900

    별 찍기 - 15(10990) 성공

---
+++ src/baekjoon/p10990/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.p10990;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N - i - 1; j++)
+				bw.write(" ");
+			bw.write("*");
+			if (i > 0) {
+				for (int j = 0; j < 2 * i - 1; j++)
+					bw.write(" ");
+				bw.write("*");
+			}
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+}
