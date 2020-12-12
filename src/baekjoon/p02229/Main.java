5152b3d6e8c6c5ecfee56280e68670f191bd7741
Sion <ygonepiece@naver.com>
2020-12-12 19:38:22 +0900

    조 짜기(2229) 성공

---
+++ src/baekjoon/p02229/Main.java
@@ -0,0 +1,35 @@
+package baekjoon.p02229;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N; static int[] S;
+	static int[] cache;
+
+	static int dp(int start) {
+		if (start >= N) return 0;
+		if (cache[start] != -1) return cache[start];
+		int ret = 0; int min = 10000; int max = 0;
+		for (int end = start; end < N; end++) {
+			min = Math.min(min, S[end]); max = Math.max(max, S[end]);
+			ret = Math.max(ret, max - min + dp(end + 1));
+		}
+		return cache[start] = ret;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		S = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++)
+			S[i] = Integer.parseInt(st.nextToken());
+		cache = new int[N]; Arrays.fill(cache, -1);
+		System.out.println(dp(0));
+	}
+
+}
