5215f668745cc3e9d96e4d5137f5d16349cb04db
Sion <ygonepiece@naver.com>
2020-12-12 14:40:35 +0900

    벽장문의 이동(2666) 성공

--- src/baekjoon/p02666/Main.java
+++ src/baekjoon/p02666/Main.java
@@ -10,19 +10,16 @@ public class Main {
 	static int N; static int M; static int[] S;
 	static int[][][] cache;
 
+	// 빈 벽장문이 (i, j)에 있을 때 k번째 벽장문을 쓰기 위해서 이동해야하는 최소 이동 수
 	static int dp(int i, int j, int k) {
 		if (k == M) {return 0;}
 		if (cache[i][j][k] != -1) {return cache[i][j][k];}
-		if (i + 1 == S[k] || j + 1 == S[k])
+		if (i == S[k] || j == S[k])
 			return cache[i][j][k] = dp(i, j, k + 1);
-		int min = N;
-		if (i - 1 > 0) min = Math.min(min, 1 + dp(i - 1, j, k));
-		if (i + 1 < j ) {
-			min = Math.min(min, 1 + dp(i + 1, j, k));
-			min = Math.min(min, 1 + dp(i, j - 1, k));
-		}
-		if (j + 1 < N) min = Math.min(min, 1 + dp(i, j + 1, k));
-		return cache[i][j][k] = min;
+		// 왼쪽, 오른쪽 빈 벽장문을 이동시키는 경우
+		return cache[i][j][k] =
+		Math.min(Math.abs(S[k] - i) + dp(S[k], j, k + 1),
+				 Math.abs(S[k] - j) + dp(i, S[k], k + 1));
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -33,12 +30,12 @@ public class Main {
 		int b = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(br.readLine()); S = new int[M];
 		for (int i = 0; i < M; i++)
-			S[i] = Integer.parseInt(br.readLine());
+			S[i] = Integer.parseInt(br.readLine()) - 1;
 		cache = new int[N][N][M];
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < N; j++)
 				Arrays.fill(cache[i][j], -1);
-		System.out.println(dp(a, b, 0));
+		System.out.println(dp(a - 1, b - 1, 0));
 	}
 
 }
