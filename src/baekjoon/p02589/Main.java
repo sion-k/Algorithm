7907b0fdefd21474089f8a7bafe413722acac732
Sion <ygonepiece@naver.com>
2020-12-13 19:59:20 +0900

    보물섬(2589) 메모리 초과

--- src/baekjoon/p02589/Main.java
+++ src/baekjoon/p02589/Main.java
@@ -3,15 +3,15 @@ package baekjoon.p02589;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N; static int M;
-	static boolean[][] LAND;
+	static boolean[][] MAP;
 
-	// 상하좌우 이동
 	static final int[] dy = {-1, 1, 0, 0};
 	static final int[] dx = {0, 0, -1, 1};
 
@@ -20,25 +20,23 @@ public class Main {
 	}
 
 	// 정점 start에서 bfs해서 최대 거리 반환
-	static int BFS(int y, int x) {
+	static int BFS(int sy, int sx) {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {y, x});
-		boolean[][] BOOKED = new boolean[N][M];
-		BOOKED[y][x] = true;
-		int[][] DIST = new int[N][M];
-		DIST[y][x] = 0;
+		q.add(new int[] {sy, sx});
+		int[][] dist = new int[N][M];
+		for (int i = 0; i < N; i++) Arrays.fill(dist[i], -1);
+		dist[sy][sx] = 0;
 
 		int max = 0;
 		while(!q.isEmpty()) {
 			int[] here = q.poll();
-			for (int next = 0; next < 4; next++) {
-				int ty = here[0] + dy[next]; int tx = here[1] + dx[next];
-				if(inRange(ty, tx) && LAND[ty][tx] && !BOOKED[ty][tx]) {
-					q.offer(new int[] {ty, tx});
-					BOOKED[ty][tx] = true;
-					DIST[ty][tx] = DIST[here[0]][here[1]] + 1;
-					max = Math.max(max, DIST[ty][tx]);
-				}
+			int y = here[0]; int x = here[1];
+			for (int d = 0; d < 4; d++) {
+				int ny = y + dy[d]; int nx = x + dx[d];
+				if (!inRange(ny, nx) || !MAP[ny][nx]) continue;
+				q.offer(new int[] {ny, nx});
+				dist[ny][nx] = dist[y][x] + 1;
+				max = Math.max(max, dist[ny][nx]);
 			}
 		}
 		return max;
@@ -49,18 +47,18 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		LAND = new boolean[N][M];
+		MAP = new boolean[N][M];
 		for (int i = 0; i < N; i++) {
 			String row = br.readLine();
 			for (int j = 0; j < M; j++) {
-				LAND[i][j] = (row.charAt(j) == 'L');
+				MAP[i][j] = (row.charAt(j) == 'L');
 			}
 		}
 
 		int max = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < M; j++) {
-				if (LAND[i][j]) {max = Math.max(max, BFS(i, j));}
+				if (MAP[i][j]) {max = Math.max(max, BFS(i, j));}
 			}
 		}
 		System.out.println(max);
