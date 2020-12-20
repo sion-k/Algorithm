a3804ff8d25fd5428a55e413add70d20f9b2b56f
Sion <ygonepiece@naver.com>
2020-12-20 19:58:04 +0900

    오븐 시계(2525) 성공

--- src/baekjoon/p10699/Main.py
+++ src/baekjoon/p10699/Main.py
@@ -1,1 +1,6 @@
-print("-".join(["2020", "12", "20"]))
+A, B = map(int, input().split())
+C = int(input())
+minute = 60 * A + B + C
+if minute >= (60 * 24) :
+    minute -= 60 * 24
+print("%d %d" % (minute // 60, minute % 60))
