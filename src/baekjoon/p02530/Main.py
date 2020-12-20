d0e0f5ce4d80b34578b9f2a742137d6e4ac3b84b
Sion <ygonepiece@naver.com>
2020-12-21 04:58:09 +0900

    인공지능 시계(2580) 성공

--- src/baekjoon/p02530/Main.py
+++ src/baekjoon/p02530/Main.py
@@ -1,4 +1,4 @@
 A, B, C = map(int, input().split())
 D = int(input())
-S = 60 * 60 * A + 60 * B + C + D
-print((S // 60 * 60) % 24, (S // 60) % 60, S % 60)
+S = (3600 * A + 60 * B + C + D) % 86400
+print(S // 3600, (S % 3600) // 60, S % 60)
