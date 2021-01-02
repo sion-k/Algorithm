a3738c0a17aed0c23a820a324ec562a4c46846ac
SION <ygonepiece@naver.com>
2021-01-02 23:49:46 +0900

    가장 가까운 세 사람의 심리적 거리(20529) 실패

--- src/baekjoon/p20529/Main.java
+++ src/baekjoon/p20529/Main.java
@@ -5,56 +5,29 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-	static final char[] MBTI = { 'E', 'S', 'T', 'J' };
-	static int[] count;
-	static final int INF = 12;
-
-	// 비트마스킹을 사용해서 16가지의 MBTI유형을 4자리 이진수로 표현
-	static int getIndex(String mbti) {
-		int bin = 0;
-		for (int i = 0; i < 4; i++)
-			if (mbti.charAt(i) == MBTI[i])
-				bin += ((int) Math.pow(2, i));
-		return bin;
-	}
+	static final String[] TYPE = { "ISTJ", "ISFJ", "INFJ", "INTJ", "ISTP", "ISFP", "INFP", "INTP", "ESTP", "ESFP",
+			"ENFP", "ENTP", "ESTJ", "ESFJ", "ENFJ", "ENTJ" };
 
 	// 두 유형 사이의 심리적 거리
-	static int dist(int i, int j) {
-		String s1 = String.format("%04d", Integer.parseInt(Integer.toBinaryString(i).toString()));
-		String s2 = String.format("%04d", Integer.parseInt(Integer.toBinaryString(j).toString()));
+	static int dist(String u, String v) {
 		int dist = 0;
-		for (int iter = 0; iter < 4; iter++)
-			if (s1.charAt(iter) != s2.charAt(iter))
+		for (int i = 0; i < 4; i++)
+			if (u.charAt(i) != v.charAt(i))
 				dist++;
 		return dist;
 	}
 
-	// count배열에서 중복을 허용해서 3개를 고르는 경우를 모두 시도한 뒤 최소 심리 거리 반환
-	static int dfs(int here, ArrayList<Integer> picked) {
-		if (here >= 16) {
-			if (picked.size() == 3)
-				return dist(picked.get(0), picked.get(1)) + dist(picked.get(1), picked.get(2))
-				+ dist(picked.get(2), picked.get(0));
-			else
-				return INF;
+	static final char[] ESTJ = {'E', 'S', 'T', 'J'};
 
-		}
-		int min = INF;
-		// 고르지 않는 경우
-		min = Math.min(min, dfs(here + 1, picked));
-		// 고르는 경우
-		if (picked.size() < 3 && count[here] > 0) {
-			count[here]--;
-			picked.add(here);
-			min = Math.min(min, dfs(here, picked));
-			picked.remove(picked.size() - 1);
-			count[here]++;
-		}
-		return min;
+	static int getBin(String s) {
+		int index = 0;
+		for (int i = 0; i < 4; i++)
+			if (s.charAt(i) != ESTJ[i])
+				index += (int)Math.pow(2, i);
+		return index;
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -63,11 +36,31 @@ public class Main {
 		int T = Integer.parseInt(br.readLine());
 		for (int c = 0; c < T; c++) {
 			int N = Integer.parseInt(br.readLine());
-			count = new int[16];
+			int[] count = new int[16];
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for (int i = 0; i < N; i++)
-				count[getIndex(st.nextToken())]++;
-			bw.write(String.valueOf(dfs(0, new ArrayList<Integer>(3))));
+			for (int i = 0; i < N; i++) {
+				int here = getBin(st.nextToken());
+				if (count[here] < 3) count[here]++;
+			}
+			int min = 12;
+			for (int i = 0; i < 16; i++) {
+				if (count[i] == 0) continue;
+				count[i]--;
+				for (int j = i; j < 16; j++) {
+					if (count[j] == 0) continue;
+					count[j]--;
+					for (int k = j; k < 16; k++) {
+						if (count[k] == 0) continue;
+						count[k]--;
+						min = Math.min(min,
+						dist(TYPE[i], TYPE[j]) + dist(TYPE[j], TYPE[k]) + dist(TYPE[k], TYPE[i]));
+						count[k]++;
+					}
+					count[j]++;
+				}
+				count[i]++;
+			}
+			bw.write(String.valueOf(min));
 			bw.newLine();
 		}
 		bw.close();
