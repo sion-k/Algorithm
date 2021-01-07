ec982790a084b8a919073cb0e38f81d443925927
SION <ygonepiece@naver.com>
2021-01-07 23:17:13 +0900

    영화감독 숌(1436) 성공

---
+++ bin/python/02P.py
@@ -0,0 +1,16 @@
+# Q1
+"""
+a, b, c = map(int, input().split())
+print("%0.2f" % ((a + b + c) / 3))
+"""
+# Q2
+"""
+print(int(input()) % 2 == 0)
+"""
+# Q3
+"""
+a, b = input().split("-")
+print(" ".join([a, b]))
+"""
+# Q5
+print("a:b:c:d".replace(":", "#"))
--- src/baekjoon/p01436/Main.java
+++ src/baekjoon/p01436/Main.java
@@ -6,17 +6,27 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+	static boolean is666(int S) {
+		boolean ret = false;
+		while (S >= 666) {
+			if (S % 1000 == 666) {ret = true; break;}
+			S /= 10;
+		}
+		return ret;
+	}
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int i = 666;
 		int cnt = 0;
 		while (cnt < N) {
-			if ((i + "").matches(".*666.*"))
+			if (is666(i))
 				cnt++;
 			i++;
 		}
-		System.out.println(i - 1);
+		i--;
+		System.out.println(i);
 	}
 
 }
