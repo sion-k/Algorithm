30371a6c9d913e488368788c2d76f25176e050b3
SION <ygonepiece@naver.com>
2021-01-12 01:00:49 +0900

    평범한 배낭(12865) 성공

--- src/baekjoon/p12865/Main.py
+++ src/baekjoon/p12865/Main.py
@@ -1,8 +1,19 @@
 N, K = map(int, input().split())
+# (무게, 가치)
 S = [()] * (N + 1)
 for i in range(1, N + 1) :
     S[i] = tuple(map(int, input().split()))
-print(S)
-dp = ((0, ) * (K + 1), ) * 2
+cache = []
+for i in range(N + 1) :
+    cache.append([-1] * (K + 1))
 
-print(dp)
+def dp(i, capacity) :
+    if i > N : return 0
+    if cache[i][capacity] != -1 : return cache[i][capacity]
+    cand = dp(i + 1, capacity)
+    if capacity >= S[i][0] :
+        cand = max(cand, S[i][1] + dp(i + 1, capacity - S[i][0]))
+    cache[i][capacity] = cand
+    return cache[i][capacity]
+
+print(dp(1, K))
