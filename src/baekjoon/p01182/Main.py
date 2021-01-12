b58dbe28d8afe17a1cbc9aca4c7ca6b0ab91d6d1
SION <ygonepiece@naver.com>
2021-01-12 11:48:56 +0900

    부분수열의 합(1182) 성공

--- src/baekjoon/p01182/Main.py
+++ src/baekjoon/p01182/Main.py
@@ -1,12 +1,14 @@
 N, K = tuple(map(int, input().split()))
-S = (0, ) + tuple(map(int, input().split()))
-pSum = [0] * (N + 1)
-for i in range(1, N + 1) :
-    pSum[i] = pSum[i - 1] + S[i]
-cnt = 0
-# 모든 [i, j] 구간에 대해서 구간 합을 구한다
-for i in range(1, N + 1) :
-    for j in range(i, N + 1) :
-        if pSum[j] - pSum[i - 1] == K :
-            cnt += 1
-print(cnt)
+S = (0,) + tuple(map(int, input().split()))
+# S[here] 까지 1개 이상 골랐는지 여부 picked와 그 합이 sum일 때
+# 합이 K인 경우의 수 반환
+def btk(here, picked, sum) :
+    global N
+    if here > N :
+        # 최소 1개 이상 골라야 함
+        if not picked : return 0
+        global K
+        return 1 if sum == K else 0
+    return btk(here + 1, picked, sum) + btk(here + 1, True, sum + S[here])
+
+print(btk(1, False, 0))
