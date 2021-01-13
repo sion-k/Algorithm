944d7b86b8f20d71cb3fc6e0b23089c9f5d2a3d4
SION <ygonepiece@naver.com>
2021-01-13 10:01:14 +0900

    숫자 카드2(10816) 성공

--- src/baekjoon/p10816/Main.java
+++ src/baekjoon/p10816/Main.java
@@ -1,17 +1,14 @@
 package baekjoon.p10816;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int N = Integer.parseInt(br.readLine());
 		// [-10000000, 10000000] -> [0, 20000000]
 		int[] S = new int[20000001];
@@ -20,11 +17,10 @@ public class Main {
 			S[Integer.parseInt(st.nextToken()) + 10000000]++;
 		int M = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < M; i++) {
-			bw.write(String.valueOf(S[Integer.parseInt(st.nextToken()) + 10000000]));
-			bw.write(" ");
-		}
-		bw.close();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < M; i++)
+			sb.append(S[Integer.parseInt(st.nextToken()) + 10000000]).append(" ");
+		System.out.println(sb);
 	}
 
 }
