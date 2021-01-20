38db55f87d62e7c232993271000dde3e581baa49
SION <ygonepiece@naver.com>
2021-01-21 00:27:48 +0900

    자물쇠(1514) 실패 2

--- src/baekjoon/p01514/Main.java
+++ src/baekjoon/p01514/Main.java
@@ -16,17 +16,16 @@ public class Main {
 	// here의 값에 diff만큼 돌린후 그 결과를 반환한다 (음수인 경우 감소하게 돌리는 것)
 	static int rotate(int here, int diff) {return (here + diff + 10) % 10;}
 
-	// S[i, )에 대해 S[i], S[i + 1], S[i + 2]에 추가로 돌려진 값이 각각 p, q, r일때
+	// S[i, )에 대해 S[i], S[i + 1], S[i + 2]의 값이 각각 p, q, r일때
 	// 자물쇠의 풀기 위한 최소 회전 횟수
 	static int dp(int i, int p, int q, int r) {
 		if (i == N) return 0;
-		if (cache[i][p][q][r] != -1) return cache[i][p][q][r];
-		int here = rotate(S[i], p);
 		// 이미 맞춰져있는 경우 넘어간다
-		if (here == G[i]) return cache[i][p][q][r] = dp(i + 1, q, r, 0);
+		if (p == G[i]) return cache[i][p][q][r] = dp(i + 1, q, r, S[i + 3]);
+		if (cache[i][p][q][r] != -1) return cache[i][p][q][r];
 		cache[i][p][q][r] = INF;
 		// S[i]에서 할 수 있는 행동은 디스크의 값을 +- 1, 2, 3변화시키는 것
-		// 그리고 1, 2, 3의 디스크를 같이 돌릴 수 있다
+		// 그리고 1, 2, 3개의 디스크를 같이 돌릴 수 있다
 		for (int diff = -3; diff <= 3; diff++) {
 			if (diff == 0) continue;
 			int np = rotate(p, diff); int nq = rotate(q, diff); int nr = rotate(r, diff);
@@ -45,19 +44,21 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		S = new int[N]; G = new int[N];
+		S = new int[N + 3]; G = new int[N + 3];
 		String line = br.readLine();
 		for (int i = 0; i < N; i++)
 			S[i] = line.charAt(i) - '0';
 		line = br.readLine();
 		for (int i = 0; i < N; i++)
 			G[i] = line.charAt(i) - '0';
+		for (int i = N; i < N + 3; i++)
+			S[i] = G[i] = 0;
 		cache = new int[N][10][10][10];
 		for (int i = 0; i < N; i++)
 			for (int j = 0; j < 10; j++)
 				for (int k = 0; k < 10; k++)
 					Arrays.fill(cache[i][j][k], -1);
-		System.out.println(dp(0, 0, 0, 0));
+		System.out.println(dp(0, S[0], S[1], S[2]));
 	}
 
 }
