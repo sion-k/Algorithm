21058184f7172e09e5052f9ec108b2b310341b70
SION <ygonepiece@naver.com>
2021-01-20 18:11:53 +0900

    제곱수의 합(1699) 시간 초과

--- src/python/SandBox.py
+++ src/python/SandBox.py
@@ -1,14 +1,10 @@
-import re
-file = open('ck_vip1.txt', 'rt', encoding='UTF8')
-html = file.read().lower()
-p = re.compile("bmw=.*[\[\(].*[\]\)]")
-m = p.findall(html)
-S = str(m[0])
-if S.find('[') != -1 :
-    S = S[S.find('[') + 1 : S.find(']')]
-else :
-    S = S[S.find('(') + 1: S.find(')')]
-ret = S.split(",")
-print(ret)
-for i in ret :
-    print(chr(int(i) - 159), end="")
+N = int(input())
+dp = [0] + [0] * N
+for i in range(1, N + 1) :
+    j = 1
+    dp[i] = i
+    while i - j * j >= 0 :
+        if dp[i] > 1 + dp[i - j * j] or dp[i] == 0:
+            dp[i] = 1 + dp[i - j * j]
+        j += 1
+print(dp[N])
