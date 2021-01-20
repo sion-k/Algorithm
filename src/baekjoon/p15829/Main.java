17e672b34881716daa5323f29809f0474e5ea844
SION <ygonepiece@naver.com>
2021-01-20 16:02:07 +0900

    Hashing(15829) 성공

---
+++ src/baekjoon/p15829/Main.java
@@ -0,0 +1,35 @@
+package baekjoon.p15829;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	static final int r = 31;
+	static final int M = 1234567891;
+	static int[] pow = new int[50];
+
+	static {
+		pow[0] = 1;
+		long ret = 1;
+		for (int i = 1; i < 50; i++) {
+			ret = (ret * r) % M;
+			pow[i] = (int)ret;
+		}
+	}
+
+	static int hashCode(String S) {
+		long ret = 0;
+		for (int i = 0; i < S.length(); i++) {
+			ret = (ret + (S.charAt(i) - 'a' + 1) * (long)pow[i]) % M;
+		}
+		return (int)ret;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		System.out.println(hashCode(br.readLine()));
+	}
+
+}
