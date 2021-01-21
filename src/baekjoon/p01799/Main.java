b0f8c3061f5a23b6a0aea6701058ec24ac6990c2
SION <ygonepiece@naver.com>
2021-01-21 17:20:10 +0900

    비숍(1799) 더 느려짐

--- src/baekjoon/p01799/Main.java
+++ src/baekjoon/p01799/Main.java
@@ -7,24 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int[][] MAP; // 비숍을 놓을 수 있는지 여부 0이어야 놓을 수 있음
-
-	// 좌상단, 우상단, 좌하단, 우하단
-	static final int[] dy = {-1, -1, 1, 1};
-	static final int[] dx = {-1, 1, -1, 1};
-
-	static boolean inRange(int y, int x) {return 0 <= y && y < N && 0 <= x && x < N;}
-
-	// MAP[y][x]의 위치에 비숍을 k == 1 두거나 가져온다 k == -1
-	static void set(int y, int x, int k) {
-		for (int d = 0; d < 4; d++) {
-			int ny = y + dy[d]; int nx = x + dx[d];
-			while (inRange(ny, nx)) {
-				MAP[ny][nx] += k;
-				ny += dy[d]; nx += dx[d];
-			}
-		}
-	}
+	static boolean[][] MAP; // 비숍을 놓을 수 있는지 여부
 
 	// m은 i번째 행에 j열에 비숍을 놓을 수 있는지 없는지에 대한 정보를 제공
 	// 0이면 놓을 수 있고 그 이상이면 놓을 수 없음
@@ -46,14 +29,14 @@ public class Main {
 	}
 
 	// MAP[i][j]부터 경우의 수, i번째 j행에 놓을 수 있는지 여부 -> m[j]
-	static int btk(int i, int j) {
+	static int btk(int i, int j, int[] m) {
 		if (i == N) return 0; // 끝에 도달한 경우 경우의 수를 하나 찾음
-		if (j == N) return btk(i + 1, 0);
-		int max = btk(i, j + 1); // 놓지 않는 경우
-		if (MAP[i][j] == 0) { // 놓을 수 있는 경우
-			set(i, j, 1);
-			max = Math.max(max, 1 + btk(i, j + 1));
-			set(i, j, -1);
+		if (j == N) return btk(i + 1, 0, update(m));
+		int max = btk(i, j + 1, m); // 놓지 않는 경우
+		if (MAP[i][j] && m[j] == 0) { // 놓을 수 있는 경우
+			m[j] = 3;
+			max = Math.max(max, 1 + btk(i, j + 1, m));
+			m[j] = 0;
 		}
 		return max;
 	}
@@ -61,13 +44,13 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		MAP = new int[N][N];
+		MAP = new boolean[N][N];
 		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			for (int j = 0; j < N; j++)
-				MAP[i][j] = st.nextToken().equals("0") ? 1 : 0;
+				MAP[i][j] = st.nextToken().equals("1");
 		}
-		System.out.println(btk(0, 0));
+		System.out.println(btk(0, 0, new int[N]));
 	}
 
 }
