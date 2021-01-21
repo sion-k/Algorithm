16f30c0ce4898f4b7514592f749f7fc5a5f8bc41
SION <ygonepiece@naver.com>
2021-01-21 23:02:22 +0900

    비숍(1799) 성공

--- src/baekjoon/p01799/Main.java
+++ src/baekjoon/p01799/Main.java
@@ -12,25 +12,35 @@ public class Main {
 	static boolean inRange(int y, int x) {return 0 <= y && y < N && 0 <= x && x < N;}
 
 	// 보드를 대각선으로 45도돌려서 생각한다
+	// used는 2 * N - 1크기의 놓을 수 있는지 여부를 저장한다
+	// 비숍은 검은색 칸과 흰 칸중 한쪽에만 존재할 수 있으므로 경루를 나누었다.
 	static int btk(int i, boolean[] used) {
-		if (i == 2 * N - 1) return 0;
-		int max = btk(i + 1, used);
+		if (i >= 2 * N - 1) return 0;
+		int max = 0;
 		if (i < N) {
-			for (int j = 0; j < i + 1; j++)
-				if (MAP[i][j] && !used[j]) {
+			int y = 0; int x = i; int j = N - i - 1;
+			// i번째 대각선의 모든 경우를 시도
+			while (inRange(y, x)) {
+				if (MAP[y][x] && !used[j]) {
 					used[j] = true;
-					max = Math.max(max, 1 + btk(i + 1, used));
+					max = Math.max(max, 1 + btk(i + 2, used));
 					used[j] = false;
 				}
+				y++; x--; j += 2;
+			}
 		} else {
-			for (int j = 0; j < 2 * N - i - 1; j++)
-				if (MAP[i][j] && !used[j]) {
+			int y = i - N + 1; int x = N - 1; int j = i - N + 1;
+			// i번째 대각선의 모든 경우를 시도
+			while (inRange(y, x)) {
+				if (MAP[y][x] && !used[j]) {
 					used[j] = true;
-					max = Math.max(max, 1 + btk(i + 1, used));
+					max = Math.max(max, 1 + btk(i + 2, used));
 					used[j] = false;
 				}
+				y++; x--; j += 2;
+			}
 		}
-		return max;
+		return Math.max(max, btk(i + 2, used));
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -42,25 +52,7 @@ public class Main {
 			for (int j = 0; j < N; j++)
 				MAP[i][j] = st.nextToken().equals("1");
 		}
-		boolean[] temp = new boolean[N * N];
-		int y = 0; int x = 0; int k = 1; int l = 0;
-		for (int i = 0; i < N * N; i++) {
-			temp[i] = MAP[y][x];
-			y++; x--;
-			if (!inRange(y, x)) {
-				y = l; x = k;
-				if (k + 1 < N) k++;
-				else l++;
-			}
-		}
-		MAP = new boolean[2 * N - 1][N]; int t = 0;
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < i + 1; j++)
-				MAP[i][j] = temp[t++];
-		for (int i = N; i < 2 * N - 1; i++)
-			for (int j = 0; j < 2 * N - i - 1; j++)
-				MAP[i][j] = temp[t++];
-		System.out.println(btk(0, new boolean[N]));
+		System.out.println(btk(0, new boolean[2 * N - 1]) + btk(1, new boolean[2 * N - 1]));
 	}
 
 }
