f1cad43d25957dc43b41ab82ef68d22eb3032c65
SION <ygonepiece@naver.com>
2021-01-21 12:15:33 +0900

    제곱수의 합(1699) 성공(PyPy3로 제출)

--- src/python/SandBox.py
+++ src/python/SandBox.py
@@ -1,10 +1,9 @@
 N = int(input())
 dp = [0] + [0] * N
 for i in range(1, N + 1) :
-    j = 1
     dp[i] = i
-    while i - j * j >= 0 :
-        if dp[i] > 1 + dp[i - j * j] or dp[i] == 0:
-            dp[i] = 1 + dp[i - j * j]
+    j = 1
+    while j * j <= i :
+        dp[i] = min(dp[i], 1 + dp[i - j * j])
         j += 1
 print(dp[N])
