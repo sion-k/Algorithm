55660f51e7c68a279accf6b75cf6aab22626628e
SION <ygonepiece@naver.com>
2021-01-26 16:02:54 +0900

    벽장문의 이동(2666) 2차원 dp 구현 실패

--- bin/python/SandBox.py
+++ bin/python/SandBox.py
@@ -1,9 +1,29 @@
-L = int(input())
-dp = [0] + [0] * L
-for i in range(1, L + 1) :
-    dp[i] = i
-    j = 1
-    while j * j <= i :
-        dp[i] = min(dp[i], 1 + dp[i - j * j])
-        j += 1
-print(dp[L])
+# 벽장문의 이동
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+order = []
+_open = list(map(int, input().split()))
+
+_order = input()
+while _order!='':
+    order.append(int(_order))
+    _order = input()
+
+def dp(o1, o2, n):
+    global order
+    if len(order)==n:
+        return [0]
+    if o1==order[n] or o2==order[n]:
+        return [min(dp(o1, o2, n+1))]
+    result1 = 1
+    result2 = 1
+    o2_1 = o2 + (1 if o2<order[n] and o1<o2 else (-1 if o2>order[n] and o1>o2 else 0))
+    o1_2 = o1 + (1 if o1<order[n] and o2<o1 else (-1 if o1>order[n] and o2>o1 else 0))
+    if o2_1!=o2:
+        result1 += 1
+    if o1_2!=o1:
+        result2 += 1
+    return [result1+min(dp(order[n], o2_1, n+1)), result2+min(dp(o1_2, order[n], n+1))]
+print(min(dp(_open[0], _open[1], 0)))
