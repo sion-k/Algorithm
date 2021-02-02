fedbf08c987075715f9a2aff31ac943c0592e5bc
SION <ygonepiece@naver.com>
2021-02-02 15:26:06 +0900

    05-2 모듈

--- src/python/05.py
+++ src/python/05.py
@@ -9,10 +9,14 @@ class Calculator :
 a = Calculator(1, 1)
 print(a.add())
 
+# UnionFind 구현해보면서 연습
 class UnionFind :
     def __init__(self, n):
         self.parent = [i for i in range(n)]
         self.rank = [0] * n
+
+    def __str__(self):
+        return str(self.parent)
 
     def union(self, u, v):
         u = self.find(u)
@@ -27,6 +31,21 @@ class UnionFind :
         self.parent[u] = self.find(self.parent[u])
         return self.parnet[u]
 
-set = UnionFind(10)
-print(set.parent)
-print(set.rank)
+print(UnionFind(10))
+# self 쓰는거 너무 불편한데??
+
+# 클래스 변수
+# 자바의 static field와 동일하다 자바의 필드처럼 쓰면 무조건 이게됨
+
+# 05-2 모듈
+# 함수나 변수 또는 클래스를 모아놓은 파일. 라이브러리인듯
+# import는 같은 경로에 있는 모듈 혹은
+# 파이썬 라이브러리 디렉터리의 모듈만 불러오기 가능
+# import ReversedPrime # 04.py는 불러올 수가 없다. 이름 잘지어야겠네
+# print(ReversedPrime.isPrime(3)) 소스 파일 자체를 불러오는건가?
+from sys import stdin
+
+# if __name__ == "__main__":의 의미
+# import 하자 마자 그냥 그 소스파일이 실행되는 것을 방지하기 위해
+# import해서 사용하는 경우는 실행되지 않게 만든다
+# __name__은 내부적으로 사용하는 변수 이름
