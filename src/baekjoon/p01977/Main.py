0247a8ec7d912f73ee6da950031c14093869c61e
SION <ygonepiece@naver.com>
2021-02-02 15:52:53 +0900

    완전제곱수(1977) 성공(리스트 내포를 사용하여 필요한 범위의 제곱수만 계산

--- src/baekjoon/p01977/Main.py
+++ src/baekjoon/p01977/Main.py
@@ -1,14 +1,4 @@
 M = int(input())
 N = int(input())
-sqr = [False] * 10001
-
-for i in range(1, 101) : sqr[i * i] = True
-S = []
-for i in range(M, N + 1) :
-    if sqr[i] : S.append(i)
-
-if S :
-    print(sum(S))
-    print(min(S))
-else :
-    print(-1)
+S = [i * i for i in range(101) if M <= i * i <= N]
+print("%d\n%d" % (sum(S), S[0]) if S else -1)
