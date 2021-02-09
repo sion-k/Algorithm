f9a5e063330875981db57620da5c154c05954270
SION <ygonepiece@naver.com>
2021-02-09 18:51:49 +0900

    경로 찾기(1513) 구현 실패

--- src/baekjoon/p01513/Main.java
+++ src/baekjoon/p01513/Main.java
@@ -1,9 +1,41 @@
 package baekjoon.p01513;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main {
+	static int N, M, C;
+	// (y, x)위치에 오락실에 몇번째 오락실이 존재하는지 저장, 없으면 0
+	static int[][] MAP;
+	static int[][][] cache;
+
+	static final int MOD = 1000007;
+
+	static boolean inRange(int y, int x) {
+		return 1 <= y && y <= N && 1 <= x && x <= M;
+	}
+
+	// (y, x)에서 마지막으로 k번째 오락실을 방문했을 때,
+	// (N, M)까지 C개의 오락실을 방문하는 경로의 수를 반환
+	static int dp(int y, int x, int k) {
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		MAP = new int[N + 1][M + 1];
+		for (int i = 1; i <= C; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int y = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			MAP[y][x] = i;
+		}
 
 	}
 
