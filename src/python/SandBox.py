04ee9aebd173241ecf724fc7e291c69e5ce705a8
SION <ygonepiece@naver.com>
2021-02-11 16:59:25 +0900

    리모컨(1107) 실패

--- src/python/SandBox.py
+++ src/python/SandBox.py
@@ -1,53 +1,69 @@
-# 벽 부수고 이동하기
-import sys
-from collections import deque
+# 리모컨
+_now = 100
+N = list(input().strip())
+_N = int(''.join(N))
+M = int(input())
+b = [True for i in range(10)]
+_input = ([] if M==0 else list(map(int, input().split())))
+_min = 10
+_max  = -1
 
-input = sys.stdin.readline
+for i in _input:
+    b[i%10] = False
 
-# get input & make maze
-(N, M) = list(map(int, input().split()))
-maze = [[2 for i in range(M + 2)]]
-for _ in range(N):
-    temp = list(map(int, list(input().strip())))
-    temp.insert(0, 2)
-    temp.append(2)
-    maze.append(temp)
-maze.append([2 for i in range(M + 2)])
+for i in range(1, 10):
+    if b[i]:
+        if _min>i:
+            _min = i
+        if _max<i:
+            _max = i
 
-# compute with BFS
-queue = deque([(1, 1, True, 1)])  # n, m, can break, time
+if _now==_N or M==10:               # only use +, - buttons
+    print(abs(_now-_N))
+elif M==0:                          # use all buttons
+    print(min((len(N)), abs(_now-_N)))
+else:                               # use some number buttons and +, - buttons
+    _next = 0
+    if N[0]=='1':       # have to start low digit
+        if len(N)==1:
+            _next = _min
+        else:
+            for i in range(len(N)-1):
+                _next *= 10
+                _next += _max
+    else:
+        _next = _min
+        for i in range(len(N)-1):
+            _next *= 10
+            if not b[0]:
+                _next += _min
 
-result = -1
-visited = [[False for m in range(M + 2)] for n in range(N + 2)]
-visited[1][1] = True
+    # if only use +, - buttons is more effective than use number buttons
+    if abs(_now-_N)<=(abs(_next-_N)+len(str(_next))):
+        print(abs(_now-_N))
+    else:
+        while abs(_now-_N)>abs(_next-_N):
+            _now = _next
+            _temp = _next
+            _count = True
+            _next = 0
+            ten = 1
+            for i in range(len(str(_now))):
+                index = _temp%10
+                _temp //= 10
+                if index==_max:
+                    _next += (0 if b[0] else _min)*ten
+                else:
+                    for j in range(index+1, 10):
+                        if b[j]:
+                            index = j
+                            break
+                    _next += index*ten
+                    _next += _temp*ten*10
+                    _count = False
+                    break
+                ten *= 10
+            if _count:
+                _next += ten*_min
 
-
-def bfs(n, m, b, v, t):
-    if not visited[n][m]:
-        if maze[n][m] == t:
-            if t == 1:
-                if b:
-                    queue.append((n, m, False, v + 1))
-            else:
-                queue.append((n, m, b, v + 1))
-            visited[n][m] = True
-
-
-while len(queue) > 0:
-    (n, m, b, v) = queue.popleft()
-    before = len(queue)
-    if n == N and m == M:
-        result = v
-        break
-
-    bfs(n - 1, m, b, v, 0)
-    bfs(n + 1, m, b, v, 0)
-    bfs(n, m - 1, b, v, 0)
-    bfs(n, m + 1, b, v, 0)
-
-    bfs(n - 1, m, b, v, 1)
-    bfs(n + 1, m, b, v, 1)
-    bfs(n, m - 1, b, v, 1)
-    bfs(n, m + 1, b, v, 1)
-
-print(result)
+        print(abs(_now-_N)+len(str(_now)))
