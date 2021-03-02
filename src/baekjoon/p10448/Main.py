bbab9664493dcb6dfac72be69a77df597c6b9be5
sion-k <ygonepiece@naver.com>
2021-03-02 17:35:24 +0900

    유레카 이론(10448) 성공(이전 정보 추가)

--- src/baekjoon/p10448/Main.py
+++ src/baekjoon/p10448/Main.py
@@ -1,12 +1,12 @@
 # 1부터 n까지의 합반환
 def sum(n) : return n * (n + 1) // 2
 
-# 자연수 k가 n개의 삼각수의 합으로 표현 가능한지 여부
-def check(k, n):
+# 마지막으로 sum(last)를 골랐을 때, 자연수 k가 n개의 삼각수의 합으로 표현 가능한지 여부
+def check(k, n, last = 1):
     if n == 0 : return k == 0
-    i = 1
+    i = last
     while sum(i) <= k :
-        if check(k - sum(i), n - 1) : return True
+        if check(k - sum(i), n - 1, i) : return True
         i += 1
     return False
 
