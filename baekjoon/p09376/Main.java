2dc1da9b5572da1a7a4d9889bbbf25f5427ea2d7
sion-k <ygonepiece@naver.com>
2021-06-15 20:48:39 +0900

    탈옥(9376) 성공

--- baekjoon/p09376/Main.java
+++ baekjoon/p09376/Main.java
@@ -1,95 +1,88 @@
 package baekjoon.p09376;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N; static int M;
-	static char[][] MAP;
-	static int[][][] DIST;
-
-	static final int[] dy = {-1, 1, 0, 0};
-	static final int[] dx = {0, 0, -1, 1};
-
-	static boolean inRange(int y, int x) {
-		return 0 <= y && y < N && 0 <= x && x < M;
-	}
-
-	static void BFS(int sy, int sx, int p) {
-		Deque<int[]> q = new LinkedList<>();
-		q.offer(new int[] {sy, sx});
+	static int N, M;
+	static char[][] S;
+	
+	static final int[] dy = { -1, 1, 0, 0 };
+	static final int[] dx = { 0, 0, -1, 1 };
+	
+	static final int INF = 10000;
+	
+	static boolean inRange(int y, int x) { return 0 <= y && y < N && 0 <= x && x < M; }
+	
+	static int[][] dijkstra(int[] s) {
+		PriorityQueue<Pair> pq = new PriorityQueue<>();
+		pq.offer(new Pair(s[0], s[1], 0));
 		int[][] dist = new int[N][M];
-		for (int i = 0; i < N; i++) {Arrays.fill(dist[i], -1);}
-		dist[sy][sx] = 0;
-		while (!q.isEmpty()) {
-			int[] here = q.poll();
-			int y = here[0]; int x = here[1];
-			for (int m = 0; m < 4; m++) {
-				int ny = y + dy[m]; int nx = x + dx[m];
-				if (!inRange(ny, nx) || MAP[ny][nx] == '*' || dist[ny][nx] != -1) {continue;}
-				if (MAP[ny][nx] == '.') {
-					dist[ny][nx] = dist[y][x];
-					q.offerFirst(new int[] {ny, nx});
-				} else if (MAP[ny][nx] == '#') {
-					dist[ny][nx] = dist[y][x] + 1;
-					q.offerLast(new int[] {ny, nx});
+		for (int i = 0; i < N; i++) Arrays.fill(dist[i], INF);
+		dist[s[0]][s[1]] = 0;
+		while (!pq.isEmpty()) {
+			Pair p = pq.poll();
+			int y = p.y; int x = p.x; int d = p.d;
+			if (dist[y][x] < d) continue;
+			for (int n = 0; n < 4; n++) {
+				int ny = y + dy[n]; int nx = x + dx[n];
+				if (!inRange(ny, nx) || S[ny][nx] == '*') continue;
+				int nd = d + (S[ny][nx] == '#' ? 1 : 0);
+				if (dist[ny][nx] > nd) {
+					dist[ny][nx] = nd;
+					pq.offer(new Pair(ny, nx, nd));
 				}
 			}
 		}
-		DIST[p] = dist;
+		return dist;
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-		for (int c = 0; c < T; c++) {
+		StringBuilder bw = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		while (TC-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			N = Integer.parseInt(st.nextToken()) + 2;
 			M = Integer.parseInt(st.nextToken()) + 2;
-			MAP = new char[N][M];
-			DIST = new int[3][N][M];
-			List<int[]> prisoner = new ArrayList<>();
-			for (int i = 0; i < N; i++) {Arrays.fill(MAP[i], '.');}
-			for (int i = 1; i < N - 1; i++) {
+			S = new char[N][M];
+			for (int i = 0; i < N; i++) Arrays.fill(S[i], '.');
+			int[] A = null; int[] B = null;
+			for (int i = 1; i <= N - 2; i++) {
 				String row = br.readLine();
-				for (int j = 1; j < M - 1; j++) {
-					MAP[i][j] = row.charAt(j - 1);
-					if (MAP[i][j] == '$') {prisoner.add(new int[] {i, j});}
-				}
-			}
-			for (int i = 0; i < 2; i++) {
-				int[] p = prisoner.get(i);
-				BFS(p[0], p[1], i);
-			}
-			BFS(0, 0, 2);
-			for (int k = 1; k < 3; k++) {
-				for (int i = 0; i < N; i++) {
-					for (int j = 0; j < M; j++) {
-						DIST[0][i][j] += (DIST[k][i][j]);
+				for (int j = 1; j <= M - 2; j++) {
+					S[i][j] = row.charAt(j - 1);
+					if (S[i][j] == '$') {
+						if (A == null) A = new int[] { i, j };
+						else B = new int[] { i, j };
 					}
 				}
 			}
-			int min = 10000;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (MAP[i][j] == '*') {continue;}
-					min = Math.min(min, DIST[0][i][j] + (MAP[i][j] == '#' ? -2 : 0));
-				}
-			}
-			bw.write(String.valueOf(min));
-			bw.newLine();
+			int[][][] dist = new int[3][N][M];
+			dist[0] = dijkstra(A);
+			dist[1] = dijkstra(B);
+			dist[2] = dijkstra(new int[] { 0, 0 });
+			int min = 30000;
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < M; j++)
+					min = Math.min(min, dist[0][i][j] + dist[1][i][j] + dist[2][i][j] - (S[i][j] == '#' ? 2 : 0));
+			bw.append(min).append("\n");
 		}
-		bw.close();
+		System.out.print(bw);
 	}
+	
+}
 
+class Pair implements Comparable<Pair> {
+	int y, x, d;
+	
+	Pair(int y, int x, int d) { this.y = y; this.x = x; this.d = d; }
+	
+	@Override
+	public int compareTo(Pair o) { return d - o.d; }
+	
 }
