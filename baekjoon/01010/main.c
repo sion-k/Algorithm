f9a6b0de6de26974948953b540b2465938378c63
sion-k <ygonepiece@naver.com>
2021-06-25 00:23:54 +0900

    다리 놓기(1010) 성공(DP말고 단순 계산)

--- baekjoon/01010/main.c
+++ baekjoon/01010/main.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int T, N, M;
-	int C[30][30] = {0};
-	for (int i = 0; i < 30; i++) {
-		for (int j = 0; j < i + 1; j++)
-			if (i == j || j == 0) C[i][j] = 1;
-			else C[i][j] = C[i - 1][j] + C[i - 1][j - 1];
-	}
+	int T, N, M, r;
 	scanf("%d", &T);
 	while (T--) {
 		scanf("%d %d", &N, &M);
-		printf("%d\n", C[M][N]);
+		r = 1;
+		for (int i = 1; i <= N; i++) r = r * (M - i + 1) / i;
+		printf("%d\n", r);
 	}
-	return 0;
 }
