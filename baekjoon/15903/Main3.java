19df3583be51e15fb74d27932d6dd46dc1a22101
sion-k <ygonepiece@naver.com>
2021-07-22 23:03:13 +0900

    카드 합체 놀이(15903) 성공

---
+++ baekjoon/15903/Main3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		PriorityQueue<Long> pq = new PriorityQueue<>();
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) pq.offer(Long.parseLong(st.nextToken()));
+		for (int i = 0; i < M; i++) {
+			long t = pq.poll() + pq.poll();
+			pq.offer(t); pq.offer(t);
+		}
+		long sum = 0;
+		while (!pq.isEmpty()) sum += pq.poll();
+		System.out.println(sum);
+	}
+
+}
