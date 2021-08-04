e9f3d5332cc37613eb3608c7d3771ffbb78f1e17
sion-k <ygonepiece@naver.com>
2021-08-05 00:04:08 +0900

    재귀함수가 뭔가요\?(17478) 성공

--- baekjoon/19590/Main.py
+++ baekjoon/19590/Main.py
@@ -3,9 +3,5 @@ input = sys.stdin.readline
 N = int(input())
 S = [int(input()) for i in range(N)]
 m = max(S)
-x = sum(S)
-ret = 0
-if len(S) == 1 : ret = m
-elif m >= x - m : ret = m - x + m
-else : ret = x % 2
-print(ret)
+s = sum(S)
+print(m - (s - m) if m > s - m else s % 2)
