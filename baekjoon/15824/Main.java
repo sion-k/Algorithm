13a8141ce82c5e7338202d7a3e2dc447f1005689
sion-k <ygonepiece@naver.com>
2021-09-05 18:27:11 +0900

    너 봄에는 캡사이신이 맛있단다(15824) 성공

--- baekjoon/15824/Main.java
+++ baekjoon/15824/Main.java
@@ -2,12 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+	static long[] cache;
+	static final int MOD = 1000000007;
+	
+	// 2^x 반환
+	static long dp(int x) {
+		if (x == 0) return 1;
+		if (cache[x] != 0) return cache[x];
+		if (x % 2 == 0) {
+			long ret = dp(x / 2);
+			return cache[x] = (ret * ret) % MOD;
+		} else {
+			return cache[x] = 2 * dp(x - 1) % MOD;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder bw = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		ArrayList<Integer> S = new ArrayList<>(N);
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		System.out.print(bw);
+		for (int i = 0; i < N; i++) S.add(Integer.parseInt(st.nextToken()));
+		Collections.sort(S);
+		cache = new long[N];
+		long sum = 0;
+		for (int i = 0; i < N; i++) {
+			long l = dp(i); long r = dp(N - i - 1);
+			sum += (S.get(i) % MOD) * l % MOD;
+			sum -= (S.get(i) % MOD) * r % MOD;
+			sum = (sum % MOD + MOD) % MOD;
+		}
+		System.out.println(sum);
 	}
 
 }
