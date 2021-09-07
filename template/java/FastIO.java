3d54cf8f177b9b620291f548824304eacee14f0c
sion-k <ygonepiece@naver.com>
2021-09-07 15:14:16 +0900

    Fast IO

--- template/java/FastIO.java
+++ template/java/FastIO.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class FastIO {
+public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
