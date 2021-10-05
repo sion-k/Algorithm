f8963808d1f79c2d13f9d02489bc1b1f5fccc3a8
sion-k <ygonepiece@naver.com>
2021-10-05 14:10:53 +0900

    사이클 게임(20040) 성공

---
+++ baekjoon/20040/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        DisjointSet ds = new DisjointSet(N);
+        int ret = 0;
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            if (ds.find(u) == ds.find(v)) { ret = i; break; }
+            ds.union(u, v);
+        }
+        System.out.println(ret);
+    }
+
+}
+class DisjointSet {
+	int[] parent; int[] rank;
+
+	public DisjointSet(int n) {
+		parent = new int[n]; for (int i = 0; i < n; i++) parent[i] = i;
+		rank = new int[n];
+	}
+
+	void union(int u, int v) {
+		u = find(u); v = find(v);
+		if (u == v) return;
+		if (rank[u] > rank[v]) { int temp = u; u = v; v = temp; }
+		parent[u] = v;
+		if (rank[u] == rank[v]) rank[v]++;
+	}
+
+	int find(int u) {
+		if (parent[u] == u) return u;
+		return parent[u] = find(parent[u]);
+	}
+
+}
