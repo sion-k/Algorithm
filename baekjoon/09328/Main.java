81b16e3a05d349621323b733856dcfc69cc69dbf
sion-k <ygonepiece@naver.com>
2021-10-09 20:23:45 +0900

    열쇠(9328) 성공

--- baekjoon/09328/Main.java
+++ baekjoon/09328/Main.java
@@ -1,74 +1,68 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N; static int M;
-	static char[][] MAP;
+	static int N, M;
+	static char[][] S;
 
-	static final int STATE = (int)(Math.pow(2, 26)) - 1;
-	static final int[] dy = {-1, 1, 0, 0};
-	static final int[] dx = {0, 0, -1, 1};
-	static boolean inRange(int y, int x) {
-		return 0 <= y && y < N && 0 <= x && x < M;
-	}
+	static final int[] dy = { -1, 1, 0, 0 };
+	static final int[] dx = { 0, 0, -1, 1 };
 
-	static int BFS(int start) {
+	static boolean inRange(int y, int x) { return 0 <= y && y < N && 0 <= x && x < M; }
+
+	// 새로 얻은 문서의 개수와 키의 개수를 반환한다.
+	static int[] bfs(boolean[] key) {
 		Queue<int[]> q = new LinkedList<>();
-		q.offer(new int[] {0, 0, start});
-		boolean[][][] booked = new boolean[N][M][STATE];
-		booked[0][0][start] = true;
-		int doc = 0;
-
+		q.offer(new int[] { 0, 0 });
+		boolean[][] booked = new boolean[N][M];
+		booked[0][0] = true;
+		int sum = 0;
+		int newKey = 0;
 		while (!q.isEmpty()) {
-			int[] here = q.poll();
-			int y = here[0]; int x = here[1]; int s = here[2];
-			if (MAP[y][x] == '$') {doc++;}
-			for (int m = 0; m < 4; m++) {
-				int ny = y + dy[m]; int nx = x + dx[m];
-				if (!inRange(ny, nx) || MAP[ny][nx] == '*' || booked[ny][nx][s]) {
-					continue;
+			int[] p = q.poll();
+			int y = p[0]; int x = p[1];
+			for (int d = 0; d < 4; d++) { 
+				int ny = y + dy[d]; int nx = x + dx[d];
+				if (!inRange(ny, nx) || S[ny][nx] == '*' || booked[ny][nx]) continue;
+				if ('A' <= S[ny][nx] && S[ny][nx] <= 'Z' && !key[S[ny][nx] - 'A']) continue;
+				if ('a' <= S[ny][nx] && S[ny][nx] <= 'z') {
+					key[S[ny][nx] - 'a'] = true;
+					newKey++;
 				}
-				if ('a' <= MAP[ny][nx] && MAP[ny][nx] <= 'z') {
-
-				} else if ('A' <= MAP[ny][nx] && MAP[ny][nx] <= 'Z') {
-
-				} else {
-
-				}
+				if (S[ny][nx] == '$') sum++;
+				q.offer(new int[] { ny, nx });
+				booked[ny][nx] = true;
+				S[ny][nx] = '.';
 			}
 		}
-		return doc;
+		if (sum == 0 && newKey == 0) return new int[0];
+		return new int[] { sum, newKey };
 	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-		for (int c = 0; c < T; c++) {
+		StringBuilder bw = new StringBuilder();
+		int TC = Integer.parseInt(br.readLine());
+		while (TC-- > 0) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			N = Integer.parseInt(st.nextToken()) + 1;
-			M = Integer.parseInt(st.nextToken()) + 1;
-			MAP = new char[N][M];
-			Arrays.fill(MAP[0], '.');
-			for (int i = 1; i <= N; i++) {
-				Arrays.fill(MAP[i], '.');
-				String row = br.readLine();
-				for (int j = 1; j <= M; j++) {
-					MAP[i][j] = row.charAt(j - 1);
-				}
+			N = Integer.parseInt(st.nextToken()) + 2;
+			M = Integer.parseInt(st.nextToken()) + 2;
+			S = new char[N][M];
+			for (int i = 0; i < N; i++) Arrays.fill(S[i], '.');
+			for (int i = 1; i < N - 1; i++) {
+				String line = br.readLine();
+				for (int j = 1; j < M - 1; j++) S[i][j] = line.charAt(j - 1);
 			}
-			int state = Integer.parseInt(br.readLine());
-			bw.write(String.valueOf(BFS(state)));
-			bw.newLine();
+			boolean[] key = new boolean[26];
+			String K = br.readLine();
+			if (!K.equals("0")) {
+				for (int i = 0; i < K.length(); i++) key[K.charAt(i) - 'a'] = true;
+			}
+			int sum = 0; int[] p = new int[2];
+			while ((p = bfs(key)).length != 0) sum += p[0];
+			bw.append(sum).append("\n");
 		}
-		bw.close();
+		System.out.print(bw);
 	}
 
 }
