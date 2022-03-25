b34d9eb4cbc3da1c3e55f7471a3bbee1cd1016a7
sion-k <ygonepiece@naver.com>
2022-03-25 23:26:28 +0900

    1998년생인 내가 태국에서는 2541년생\?\!(18108)

---
+++ baekjoon/18108/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y)                                                         \
+    cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_tuple(x, y, z)                                                     \
+    cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", "    \
+         << (y) << ", " << (z) << ")\n"
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+#define OPEN(t) freopen("data.txt", (t), stdin)
+using namespace std;
+
+int main() {
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+    int y;
+    cin >> y;
+    cout << y - (2541 - 1998) << "\n";
+}
