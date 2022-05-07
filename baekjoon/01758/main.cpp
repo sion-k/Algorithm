594fd1fa5c1e8ab5968748f249fe38fa8f265c27
sion-k <ygonepiece@naver.com>
2022-05-07 12:03:50 +0900

    알바생 강호(1758)

---
+++ baekjoon/01758/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+#define OPEN(t) freopen("data.txt", (t), (t == "r" ? stdin : stdout))
+#define FAST() cin.tie(0);\
+    cin.sync_with_stdio(0)
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y)                                                         \
+    cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_triplet(x, y, z)                                                   \
+    cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", "    \
+         << (y) << ", " << (z) << ")\n"
+#define deb_tuple(s)                                                           \
+    for (int i = 0; i < SIZE(s); i++)                                          \
+        cout << s[i] << " \n"[i == SIZE(s) - 1];
+using namespace std;
+
+int main() {
+    FAST();
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) {
+        cin >> x;
+    }
+    sort(ALL(a));
+    reverse(ALL(a));
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += max(a[i] - i, 0);
+    }
+    cout << sum << "\n";
+}
