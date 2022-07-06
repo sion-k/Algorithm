8cb5175fb4a68bfef44a8c2096d8ae78326803cc
sion-k <axiom0510@gmail.com>
2022-07-06 18:09:43 +0900

    우체국(2141)

---
+++ baekjoon/02141/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+#define OPEN(t) freopen("data.txt", (t), (t == "r" ? stdin : stdout))
+#define FAST() cin.tie(0)->sync_with_stdio(0)
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y)                                                         \
+    cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_triplet(x, y, z)                                                   \
+    cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", "    \
+         << (y) << ", " << (z) << ")\n"
+#define deb_tuple(s)                                                           \
+    for (int i = 0; i < SIZE(s); i++)                                          \
+        cout << s[i] << " \n"[i == SIZE(s) - 1];
+using namespace std;
+
+int main() {
+    FAST();
+    int n;
+    cin >> n;
+    long long sum = 0;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i].first >> a[i].second;
+        sum += a[i].second;
+    }
+    sort(ALL(a));
+    int here = 0;
+    long long left = 0, right = sum - a[here].second;
+    while (here + 1 < n && left + a[here].second < right) {
+        left += a[here].second;
+        right -= a[here + 1].second;
+        here++;
+    }
+    cout << a[here].first << "\n";
+}
