bd5f072dcb0aa2cc640845ca9e5198847bd32c81
sion-k <axiom0510@gmail.com>
2022-07-27 00:50:16 +0900

    치킨치킨치킨(16439)

---
+++ baekjoon/16439/main.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+#define OPEN(t) freopen("data.txt", (t), (t == "r" ? stdin : stdout))
+#define FAST() cin.tie(0)->sync_with_stdio(0)
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y)                                                         \
+    cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_triplet(x, y, z)                                                   \
+    cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", "    \
+         << (y) << ", " << (z) << ")\n"
+#define deb_tuple(s)                                                           \
+    for (int i = 0; i < SIZE(s); i++)                                          \
+        cout << s[i] << " \n"[i == SIZE(s) - 1];
+using namespace std;
+
+int main() {
+    FAST();
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int max = 0;
+    for (int i = 0; i < m; i++) {
+        for (int j = i + 1; j < m; j++) {
+            for (int k = j + 1; k < m; k++) {
+                int sum = 0;
+                for (int y = 0; y < n; y++) {
+                    sum += ::max(a[y][i], ::max(a[y][j], a[y][k]));
+                }
+                max = ::max(max, sum);
+            }
+        }
+    }
+    cout << max << "\n";
+}
