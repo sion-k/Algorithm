bef13cb895ffe2dd1e93a0cf55a2c861c25f7970
sion-k <ygonepiece@naver.com>
2022-12-02 13:27:22 +0900

    ROT13(11655)

---
+++ baekjoon/11655/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define FAST() cin.tie(0)->sync_with_stdio(0)
+#define OPEN(t) freopen("data.txt", (t), (t == "r" ? stdin : stdout))
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y) cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_triplet(x, y, z) cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", " << (y) << ", " << (z) << ")\n"
+#define deb_tuple(s) for (int i = 0; i < SIZE(s); i++) cout << s[i] << " \n"[i == SIZE(s) - 1];
+
+using namespace std;
+
+int main() {
+    FAST();
+
+    string s;
+    getline(cin, s);
+
+    for (int i = 0; i < SIZE(s); i++) if ('a' <= s[i] && s[i] <= 'z' || 'A' <= s[i] && s[i] <= 'Z') {
+        int base = 'a' <= s[i] && s[i] <= 'z' ? 'a' : 'A';
+
+        s[i] = (s[i] - base + 13) % 26;
+        s[i] += base;
+    }
+
+    cout << s << "\n";
+}
