4bda60edb9c301ba9f52dbd91f0ba21ea5450581
sion-k <ygonepiece@naver.com>
2023-02-17 18:57:46 +0900

    Passport Control

---
+++ baekjoon/16288/main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#define FAST() cin.tie(0)->sync_with_stdio(0)
+#define OPEN(t) freopen("data.txt", (t), (t == "r" ? stdin : stdout))
+#define ALL(x) (x).begin(), (x).end()
+#define SIZE(x) (int)((x).size())
+
+#define deb(x) cout << #x << " : " << (x) << "\n"
+#define deb_pair(x, y) cout << "(" << #x << ", " << #y << ") : (" << (x) << ", " << (y) << ")\n"
+#define deb_triplet(x, y, z) cout << "(" << #x << ", " << #y << ", " << #z << ") : (" << (x) << ", " << (y) << ", " << (z) << ")\n"
+#define deb_tuple(s) \
+    cout << "["; \
+    for (int __i = 0; __i < SIZE(s); __i++) { \
+        cout << s[__i]; \
+        if (__i != SIZE(s) - 1) cout << ", "; \
+    } \
+    cout << "]\n";
+
+using namespace std;
+
+int main() {
+    FAST();
+
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> a(n);
+    for (auto& x : a) {
+        cin >> x;
+    }
+
+    int c = 0;
+    for (int i = 0; i < n; i++) if (a[i]) {
+        int p = a[i];
+        c++;
+
+        for (int j = i; j < n; j++) {
+            if (p <= a[j]) {
+                p = a[j];
+                a[j] = 0;
+            }
+        }
+    }
+
+    cout << (c <= k ? "YES" : "NO") << "\n";
+}
